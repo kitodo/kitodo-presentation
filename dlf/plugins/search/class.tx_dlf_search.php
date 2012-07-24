45d1d9295863365e981350a35011860efa781f51
Henrik Lochmann <dev@mentalmotive.com>
2012-07-24 15:16:02 +0200

    - 'thumbnail_usi' > 'thumbnail'

--- dlf/plugins/search/class.tx_dlf_search.php
+++ dlf/plugins/search/class.tx_dlf_search.php
@@ -143,7 +143,7 @@ class tx_dlf_search extends tx_dlf_plugin {
 						'place' => (is_array($doc->place) ? $doc->place : array ($doc->place)),
 						'type' => (is_array($doc->type) ? $doc->type : array ($doc->type)),
 						'subparts' => (!empty($toplevel[$doc->uid]['subparts']) ? $toplevel[$doc->uid]['subparts'] : array ()),
-						'thumbnail' => $doc->thumbnail_usi
+						'thumbnail' => $doc->thumbnail
 					);
 
 				} else {
