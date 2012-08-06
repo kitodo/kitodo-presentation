5df6c3f1464be73773ae6a5b163f175abf2b1de8
Sebastian Meyer <sebastian.meyer@slub-dresden.de>
2012-08-06 18:35:39 +0200

    Fix Bug #995907 OAI-PMH interface only is only able to output METS

--- dlf/plugins/oai/class.tx_dlf_oai.php
+++ dlf/plugins/oai/class.tx_dlf_oai.php
@@ -925,13 +925,13 @@ class tx_dlf_oai extends tx_dlf_plugin {
 
 			$Identify->appendChild($this->oai->createElementNS('http://www.openarchives.org/OAI/2.0/', 'protocolVersion', '2.0'));
 
-			$Identify->appendChild($this->oai->createElementNS('http://www.openarchives.org/OAI/2.0/', 'adminEmail', htmlspecialchars(trim(str_replace('mailto:', '', $resArray['contact'])), ENT_NOQUOTES, 'UTF-8')));
-
 			$Identify->appendChild($this->oai->createElementNS('http://www.openarchives.org/OAI/2.0/', 'earliestDatestamp', $datestamp));
 
 			$Identify->appendChild($this->oai->createElementNS('http://www.openarchives.org/OAI/2.0/', 'deletedRecord', 'transient'));
 
 			$Identify->appendChild($this->oai->createElementNS('http://www.openarchives.org/OAI/2.0/', 'granularity', 'YYYY-MM-DDThh:mm:ssZ'));
+
+			$Identify->appendChild($this->oai->createElementNS('http://www.openarchives.org/OAI/2.0/', 'adminEmail', htmlspecialchars(trim(str_replace('mailto:', '', $resArray['contact'])), ENT_NOQUOTES, 'UTF-8')));
 
 			return $Identify;
 
