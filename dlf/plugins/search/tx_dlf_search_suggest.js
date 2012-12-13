f9d3752cefb8342eaaac39816c2cca5ea8322978
Sebastian Meyer <sebastian.meyer@slub-dresden.de>
2012-12-13 18:42:21 +0100

    Append auto-completion list to div element instead of input field

--- dlf/plugins/search/tx_dlf_search_suggest.js
+++ dlf/plugins/search/tx_dlf_search_suggest.js
@@ -1,8 +1,8 @@
 $("#tx-dlf-search-query").attr({
-	autocomplete: 'off',
-	role: 'textbox',
-	'aria-autocomplete': 'list',
-	'aria-haspopup': 'true'
+	'autocomplete': "off",
+	'role': "textbox",
+	'aria-autocomplete': "list",
+	'aria-haspopup': "true"
 });
 
 $(
@@ -11,7 +11,7 @@ $(
 		$("#tx-dlf-search-query").autocomplete({
 			source: function(request, response) {
 				return $.post(
-					'/',
+					"/",
 					{
 						eID: "tx_dlf_search_suggest",
 						q: escape(request.term),
@@ -20,17 +20,17 @@ $(
 					},
 					function(data) {
 						var result = new Array();
-						$('arr[name="suggestion"] str', data).each(function(i) {
+						$("arr[name='suggestion'] str", data).each(function(i) {
 							if ($(this).text().indexOf(request.term) == 0) {
 								result.push($(this).text());
 							}
 						});
 						return response(result);
 					},
-					'xml');
+					"xml");
 			},
 			minLength: 3,
-			appendTo: "#tx-dlf-search-query"
+			appendTo: "#tx-dlf-search-suggest"
 		});
 	}
 );
