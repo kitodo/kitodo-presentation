577d276081a19b1041877359a748d637037c3431
Stefan Weil <sw@weilnetz.de>
2015-11-30 20:12:39 +0100

    Replace deprecated class t3lib_extMgm
    It was deprecated with TYPO3 6.0 and removed in TYPO3 7.0.
    The replacement \TYPO3\CMS\Core\Utility\ExtensionManagementUtility
    was added 3 years ago with TYPO3 6.0, so this patch removes
    compatibility with pre 6.0 versions.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- dlf/plugins/collection/class.tx_dlf_collection.php
+++ dlf/plugins/collection/class.tx_dlf_collection.php
@@ -231,7 +231,7 @@ class tx_dlf_collection extends tx_dlf_plugin {
 				// Add feed link if applicable.
 				if (!empty($this->conf['targetFeed'])) {
 
-					$img = '<img src="'.t3lib_extMgm::siteRelPath($this->extKey).'res/icons/txdlffeeds.png" alt="'.$this->pi_getLL('feedAlt', '', TRUE).'" title="'.$this->pi_getLL('feedTitle', '', TRUE).'" />';
+					$img = '<img src="'.\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::siteRelPath($this->extKey).'res/icons/txdlffeeds.png" alt="'.$this->pi_getLL('feedAlt', '', TRUE).'" title="'.$this->pi_getLL('feedTitle', '', TRUE).'" />';
 
 					$markerArray[$_key]['###FEED###'] = $this->pi_linkTP($img, array ($this->prefixId => array ('collection' => $resArray['uid'])), FALSE, $this->conf['targetFeed']);
 
