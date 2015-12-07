0dcca6da72aaa9256373fdaeeae8743380150035
Sebastian Meyer <sebastian.meyer@slub-dresden.de>
2015-12-07 17:08:16 +0100

    Add collection re-indexing to CLI

--- dlf/cli/class.tx_dlf_cli.php
+++ dlf/cli/class.tx_dlf_cli.php
@@ -53,6 +53,7 @@ class tx_dlf_cli extends t3lib_cli {
 
 		switch ((string) $this->cli_args['_DEFAULT'][1]) {
 
+			// (Re-)Index a single document.
 			case 'index':
 
 				// Add command line arguments.
@@ -89,6 +90,59 @@ class tx_dlf_cli extends t3lib_cli {
 
 				break;
 
+			// Re-index all documents of a collection.
+			case 'reindex':
+
+				// Add command line arguments.
+				$this->cli_options[] = array ('-coll UID', 'UID of the collection.');
+
+				$this->cli_options[] = array ('-pid UID', 'UID of the page the document should be added to.');
+
+				$this->cli_options[] = array ('-core UID', 'UID of the Solr core the document should be added to.');
+
+				// Check the command line arguments.
+				$this->cli_validateArgs();
+
+				// Get the collection.
+				$result = $GLOBALS['TYPO3_DB']->exec_SELECT_mm_query(
+					'tx_dlf_documents.uid AS uid',
+					'tx_dlf_documents',
+					'tx_dlf_relations',
+					'tx_dlf_collections',
+					'AND tx_dlf_collections.uid='.intval($this->cli_args['-coll'][0]).' AND tx_dlf_collections.pid='.intval($this->cli_args['-pid'][0]).' AND tx_dlf_relations.ident='.$GLOBALS['TYPO3_DB']->fullQuoteStr('docs_colls', 'tx_dlf_relations').tx_dlf_helper::whereClause('tx_dlf_documents').tx_dlf_helper::whereClause('tx_dlf_collections'),
+					'',
+					'',
+					''
+				);
+
+				while ($resArray = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($result)) {
+
+					// Get the document...
+					$doc =& tx_dlf_document::getInstance($resArray['uid'], $this->cli_args['-pid'][0], TRUE);
+
+					if ($doc->ready) {
+
+						// ...and save it to the database...
+						if (!$doc->save(intval($this->cli_args['-pid'][0]), intval($this->cli_args['-core'][0]))) {
+
+							$this->cli_echo('ERROR: Document '.$resArray['uid'].' not saved and indexed.'.LF, TRUE);
+
+							exit (1);
+
+						}
+
+					} else {
+
+						$this->cli_echo('ERROR: Document '.$resArray['uid'].' could not be loaded.'.LF, TRUE);
+
+						exit (1);
+
+					}
+
+				}
+
+				break;
+
 			default:
 
 				$this->cli_help();
@@ -107,7 +161,7 @@ class tx_dlf_cli extends t3lib_cli {
 		$this->cli_help = array (
 			'name' => 'Command Line Interface for Goobi.Presentation',
 			'synopsis' => '###OPTIONS###',
-			'description' => 'Currently the only task available is "index".'.LF.'Try "/PATH/TO/TYPO3/cli_dispatch.phpsh dlf index" to view more options.',
+			'description' => 'Currently the only tasks available are "index" and "reindex".'.LF.'Try "/PATH/TO/TYPO3/cli_dispatch.phpsh dlf TASK" for more options.',
 			'examples' => '/PATH/TO/TYPO3/cli_dispatch.phpsh dlf TASK -ARG1=VALUE1 -ARG2=VALUE2',
 			'options' => '',
 			'license' => 'GNU GPL - free software!',
