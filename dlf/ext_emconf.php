817c3bcdc8017009b4e7616cfe11b2b7502fe6fd
Stefan Weil <sw@weilnetz.de>
2016-01-08 21:06:27 +0100

    Update range of supported TYPO3 versions
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- dlf/ext_emconf.php
+++ dlf/ext_emconf.php
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-',
-			'typo3' => '6.2.0-6.2.99',
+			'typo3' => '6.2.0-7.9.99',
 			't3jquery' => '2.6.0-',
 		),
 		'conflicts' => array(
