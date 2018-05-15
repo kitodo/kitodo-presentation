7a2009c336da7fe76ad7212d157cccdc21dd1248
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2018-05-15 17:34:25 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,33 +9,69 @@
  * LICENSE.txt file that was distributed with this source code.
  */
 
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Kitodo.Presentation',
     'description' => 'Base plugins, modules, services and API of the Digital Library Framework. It is part of the community-based Kitodo Digitization Suite.',
-    'category' => 'fe',
-    'author' => 'Kitodo. Key to digital objects e.V.',
-    'author_email' => 'contact@kitodo.org',
-    'author_company' => 'http://www.kitodo.org/',
-    'shy' => '',
-    'priority' => '',
-    'module' => '',
+    'version' => '3.0.0',
+    'category' => 'misc',
+    'constraints' => [
+        'depends' => [
+            'php' => '5.5.0-',
+            'typo3' => '7.6.0-8.9.99'
+        ],
+        'conflicts' => [],
+        'suggests' => []
+    ],
     'state' => 'stable',
-    'internal' => '',
     'uploadfolder' => TRUE,
     'createDirs' => '',
-    'modify_tables' => '',
     'clearCacheOnLoad' => FALSE,
-    'lockType' => '',
-    'version' => '2.2.0',
-    'constraints' => array (
-        'depends' => array (
-            'php' => '5.3.7-',
-            'typo3' => '6.2.0-7.9.99',
-        ),
-        'conflicts' => array (
-        ),
-        'suggests' => array (
-        ),
-    ),
-    '_md5_values_when_last_written' => '',
-);
+    'author' => 'Sebastian Meyer',
+    'author_email' => 'sebastian.meyer@slub-dresden.de',
+    'author_company' => 'Kitodo. Key to digital objects e. V.',
+    'autoload' => [
+        'classmap' => [
+            "Classes/Cli/class.tx_dlf_cli.php",
+            "Classes/Common/class.tx_dlf_document.php",
+            "Classes/Common/class.tx_dlf_format.php",
+            "Classes/Common/class.tx_dlf_fulltext.php",
+            "Classes/Common/class.tx_dlf_helper.php",
+            "Classes/Common/class.tx_dlf_indexing.php",
+            "Classes/Common/class.tx_dlf_list.php",
+            "Classes/Common/class.tx_dlf_module.php",
+            "Classes/Common/class.tx_dlf_plugin.php",
+            "Classes/Common/class.tx_dlf_solr.php",
+            "Classes/Formats/class.tx_dlf_alto.php",
+            "Classes/Formats/class.tx_dlf_mods.php",
+            "Classes/Formats/class.tx_dlf_teihdr.php",
+            "Classes/Hooks/class.tx_dlf_doctype.php",
+            "Classes/Hooks/class.tx_dlf_em.php",
+            "Classes/Hooks/class.tx_dlf_hacks.php",
+            "Classes/Hooks/class.tx_dlf_tceforms.php",
+            "Classes/Hooks/class.tx_dlf_tcemain.php",
+            "modules/indexing/index.php",
+            "modules/newclient/index.php",
+            "plugins/audioplayer/class.tx_dlf_audioplayer.php",
+            "plugins/basket/class.tx_dlf_basket.php",
+            "plugins/collection/class.tx_dlf_collection.php",
+            "plugins/feeds/class.tx_dlf_feeds.php",
+            "plugins/listview/class.tx_dlf_listview.php",
+            "plugins/metadata/class.tx_dlf_metadata.php",
+            "plugins/navigation/class.tx_dlf_navigation.php",
+            "plugins/newspaper/class.tx_dlf_newspaper.php",
+            "plugins/oai/class.tx_dlf_oai.php",
+            "plugins/pagegrid/class.tx_dlf_pagegrid.php",
+            "plugins/pageview/class.tx_dlf_pageview.php",
+            "plugins/search/class.tx_dlf_search.php",
+            "plugins/search/class.tx_dlf_search_suggest.php",
+            "plugins/statistics/class.tx_dlf_statistics.php",
+            "plugins/toc/class.tx_dlf_toc.php",
+            "plugins/toolbox/class.tx_dlf_toolbox.php",
+            "plugins/toolbox/tools/pdf/class.tx_dlf_toolsPdf.php",
+            "plugins/toolbox/tools/fulltext/class.tx_dlf_toolsFulltext.php",
+            "plugins/toolbox/tools/imagemanipulation/class.tx_dlf_toolsImagemanipulation.php",
+            "plugins/toolbox/tools/imagedownload/class.tx_dlf_toolsImagedownload.php",
+            "plugins/validator/class.tx_dlf_validator.php"
+        ]
+    ]
+];
