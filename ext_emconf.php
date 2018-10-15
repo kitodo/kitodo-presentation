2ac6c87cb2f3746508119efac4f86e494db0cfa7
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2018-10-15 16:20:34 +0200

    Fix version constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array (
     'constraints' => array (
         'depends' => array (
             'php' => '7.0.8-',
-            'typo3' => '7.0-',
+            'typo3' => '7.0.0-',
         ),
         'conflicts' => array (
         ),
