212b8f9ed968e9976b0d827a62a818ffeb9248d3
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2019-03-13 17:59:04 +0100

    Update system requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.0.0-',
-            'typo3' => '7.6.0-8.9.99'
+            'typo3' => '8.7.0-8.7.99'
         ],
         'conflicts' => [],
         'suggests' => []
