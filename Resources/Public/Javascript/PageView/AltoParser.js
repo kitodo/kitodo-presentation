8b7437c15f729a9f1383483cd504ac118072f221
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2019-03-17 21:57:56 +0100

    Add missing javascript for page view

---
+++ Resources/Public/Javascript/PageView/AltoParser.js
@@ -0,0 +1,360 @@
+/**
+ * (c) Kitodo. Key to digital objects e.V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo and TYPO3 projects.
+ *
+ * @license GNU General Public License version 3 or later.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+/**
+ * @constructor
+ * @param {Object=} opt_imageObj
+ * @param {number=} opt_width
+ * @param {number=} opt_height
+ * @param {number=} opt_offset
+ */
+var dlfAltoParser = function(opt_imageObj, opt_width, opt_height, opt_offset) {
+
+    /**
+     * @type {Object|undefined}
+     * @private
+     */
+    this.image_ = dlfUtils.exists(opt_imageObj) ? opt_imageObj : undefined;
+
+    /**
+     * @type {number|undefined}
+     * @private
+     */
+    this.width_ = dlfUtils.exists(opt_width) ? opt_width : undefined;
+
+    /**
+     * @type {number|undefined}
+     * @private
+     */
+    this.height_ = dlfUtils.exists(opt_height) ? opt_height : undefined;
+
+    /**
+     * @type {number|undefined}
+     * @private
+     */
+    this.offset_ = dlfUtils.exists(opt_offset) ? opt_offset : undefined;
+};
+
+/**
+ * @param {number} width
+ * @param {number} height
+ * @param {number} hpos
+ * @param {number} vpos
+ * @private
+ */
+dlfAltoParser.prototype.generateId_ = function(width, height, hpos, vpos) {
+    var heigt_ = isNaN(height) ? '0' : height;
+    return '' + width + '_' + heigt_ + '_' + hpos + '_' + vpos;
+};
+
+/**
+ * Parse from an alto element a OpenLayers feature object. This function does reproduce the parsing hierarchy of this parser.
+ * @param {Element} node
+ * @return {ol.Feature}
+ * @private
+ */
+dlfAltoParser.prototype.parseAltoFeature_ = function(node) {
+    var type = node.nodeName.toLowerCase(),
+        feature;
+
+    // first parse the node as feature
+    if (type === 'printspace' || type === 'textblock' || type === 'textline' || node.hasAttribute('WIDTH')) {
+        feature = this.parseFeatureWithGeometry_(node);
+    } else {
+        var feature = new ol.Feature();
+        feature.setProperties({'type': node.nodeName.toLowerCase()});
+    };
+
+    // parse child nodes
+    if (type === 'page') {
+        // try to update the general width and height for rescaling
+        var width = feature.get('width'),
+            height = feature.get('height');
+
+        if (dlfUtils.exists(width) && dlfUtils.exists(height) && !dlfUtils.exists(this.width_) && !dlfUtils.exists(this.height_)) {
+            this.width_ = width;
+            this.height_ = height;
+        }
+
+        // add child features
+        feature.setProperties({'printspace': this.parsePrintSpaceFeature_(node)});
+    } else if (type === 'printspace') {
+        // try to update the general width and height for rescaling
+        var width = feature.get('width'),
+            height = feature.get('height');
+
+        if (dlfUtils.exists(width) && dlfUtils.exists(height) && !dlfUtils.exists(this.width_) && !dlfUtils.exists(this.height_)) {
+            this.width_ = width;
+            this.height_ = height;
+        }
+
+        // add child features
+        feature.setProperties({'textblocks': this.parseTextBlockFeatures_(node)});
+    } else if (type === 'textblock') {
+        feature.setProperties({'textlines': this.parseTextLineFeatures_(node)});
+    } else if (type === 'textline') {
+        feature.setProperties({'content': this.parseContentFeatures_(node)});
+    }
+
+    return feature;
+};
+
+/**
+ * @param {XMLDocument|string} document
+ * @return {Array.<ol.Feature>}
+ */
+dlfAltoParser.prototype.parseFeatures = function(document) {
+    var parsedDoc = this.parseXML_(document),
+        pageFeatures = [],
+        pageElements = $(parsedDoc).find('Page');
+
+    for (var i = 0; i < pageElements.length; i++) {
+        // parse page feature
+        var feature = this.parseAltoFeature_(pageElements[i]);
+
+        /**
+         * Attach function for a better access of of string elements
+         * @return {Array}
+         */
+        feature.getStringFeatures = function() {
+            var textblockFeatures = this.get('printspace').get('textblocks'),
+                stringFeatures = [];
+
+            textblockFeatures.forEach(function(textblock) {
+
+                if (textblock !== undefined && textblock.get('textlines').length > 0) {
+
+                    textblock.get('textlines').forEach(function(textline) {
+
+                        if (textline !== undefined && textline.get('content').length > 0) {
+
+                            textline.get('content').forEach(function(content) {
+
+                                if (content !== undefined && content.get('type') === 'string') {
+                                    stringFeatures.push(content);
+                                }
+
+                            });
+                        }
+
+                    });
+
+                }
+
+            });
+
+            return stringFeatures;
+        };
+
+        /**
+         * Add function for getting the text blocks
+         * @return {Array}
+         */
+        feature.getTextblocks = function() {
+            if (this.get('printspace') !== undefined && this.get('printspace').get('textblocks'))
+                return this.get('printspace').get('textblocks')
+            return [];
+        };
+
+        /**
+         * Adding function for getting the textline
+         * @return {Array}
+         */
+        feature.getTextlines = function() {
+            var textlines = [];
+            this.getTextblocks().forEach(function(textblock) {
+                textlines = textlines.concat(textblock.get('textlines'));
+            });
+            return textlines;
+        };
+
+        pageFeatures.push(feature);
+    };
+
+    return pageFeatures;
+};
+
+/**
+ * Parse from an alto element a OpenLayers feature object
+ * @param {Element} node
+ * @return {ol.Feature}
+ * @private
+ */
+dlfAltoParser.prototype.parseFeatureWithGeometry_ = function(node) {
+    var geometry = this.parseGeometry_(node),
+        width = parseInt(node.getAttribute("WIDTH")),
+        height = parseInt(node.getAttribute("HEIGHT")),
+        hpos = parseInt(node.getAttribute("HPOS")),
+        vpos = parseInt(node.getAttribute("VPOS")),
+        type = node.nodeName.toLowerCase(),
+        id = this.generateId_(width, height, hpos, vpos),
+        feature = new ol.Feature(geometry);
+
+    feature.setId(id);
+    feature.setProperties({
+        'type':type,
+        'width': width,
+        'height': height,
+        'hpos': hpos,
+        'vpos': vpos
+    });
+
+    return feature;
+};
+
+/**
+ * Parse from an alto element a OpenLayers geometry object
+ * @param {Element} node
+ * @return {ol.geom.Polygon|undefined}
+ * @private
+ */
+dlfAltoParser.prototype.parseGeometry_ = function(node) {
+    var width = parseInt(node.getAttribute("WIDTH")),
+        height = parseInt(node.getAttribute("HEIGHT")),
+        x1 = parseInt(node.getAttribute("HPOS")),
+        y1 = parseInt(node.getAttribute("VPOS")),
+        x2 = x1 + width,
+        y2 = y1 + height,
+        coordinatesWithoutScale = [[[x1, y1], [x2, y1], [x2, y2], [x1, y2], [x1, y1]]];
+
+    if (isNaN(width) || isNaN(height))
+        return undefined;
+
+    // return geometry without rescale
+    if (!dlfUtils.exists(this.image_) || !dlfUtils.exists(this.width_))
+        return new ol.geom.Polygon(coordinatesWithoutScale);
+
+    // rescale coordinates
+    var scale = this.image_.width / this.width_,
+        offset = dlfUtils.exists(this.offset_) ? this.offset_ : 0,
+        coordinatesRescale = [];
+
+    for (var i = 0; i < coordinatesWithoutScale[0].length; i++) {
+        coordinatesRescale.push([offset + ( scale * coordinatesWithoutScale[0][i][0]),
+            0 - (scale * coordinatesWithoutScale[0][i][1])]);
+    };
+
+    return new ol.geom.Polygon([coordinatesRescale]);
+};
+
+/**
+ * @param {Element} node
+ * @return {ol.Feature|undefined}
+ * @private
+ */
+dlfAltoParser.prototype.parsePrintSpaceFeature_ = function(node) {
+    var printspace = $(node).find('PrintSpace');
+
+    if (printspace.length === 0)
+        return;
+    return this.parseAltoFeature_(printspace[0]);
+};
+
+/**
+ * @param {Element} node
+ * @return {Array.<ol.Feature>}
+ * @private
+ */
+dlfAltoParser.prototype.parseTextBlockFeatures_ = function(node) {
+    var textblockElements = $(node).find('TextBlock'),
+        textblockFeatures = [];
+
+    for (var i = 0; i < textblockElements.length; i++) {
+        var feature = this.parseAltoFeature_(textblockElements[i]),
+            textlines = feature.get('textlines'),
+            fulltext = '';
+
+        // aggregated fulltexts
+        for (var j = 0; j < textlines.length; j++) {
+            fulltext += textlines[j].get('fulltext') + '\n';
+        };
+        feature.setProperties({'fulltext':fulltext});
+
+        textblockFeatures.push(feature);
+    };
+
+    return textblockFeatures;
+};
+
+/**
+ * @param {Element} node
+ * @return {Array.<ol.Feature>}
+ * @private
+ */
+dlfAltoParser.prototype.parseTextLineFeatures_ = function(node) {
+    var textlineElements = $(node).find('TextLine'),
+        textlineFeatures = [];
+
+    for (var i = 0; i < textlineElements.length; i++) {
+        var feature = this.parseAltoFeature_(textlineElements[i]),
+            fulltextElements = feature.get('content'),
+            fulltext = '';
+
+        // parse fulltexts
+        for (var j = 0; j < fulltextElements.length; j++) {
+            fulltext += fulltextElements[j].get('fulltext');
+        };
+        feature.setProperties({'fulltext':fulltext});
+
+        textlineFeatures.push(feature);
+    };
+
+    return textlineFeatures;
+};
+
+/**
+ * @param {Element} node
+ * @return {Array.<ol.Feature>}
+ * @private
+ */
+dlfAltoParser.prototype.parseContentFeatures_ = function(node) {
+    var textlineContentElements = $(node).children(),
+        textlineContentFeatures = [];
+
+    for (var i = 0; i < textlineContentElements.length; i++) {
+        var feature = this.parseFeatureWithGeometry_(textlineContentElements[i]),
+            fulltext = '';
+
+        // parse fulltexts
+        switch (textlineContentElements[i].nodeName.toLowerCase()) {
+            case 'string':
+                fulltext = textlineContentElements[i].getAttribute('CONTENT');
+                break;
+            case 'sp':
+                fulltext = ' ';
+                break;
+            case 'hyp':
+                fulltext = '-';
+                break;
+            default:
+                fulltext = '';
+        };
+        feature.setProperties({'fulltext':fulltext});
+
+        textlineContentFeatures.push(feature);
+    };
+
+    return textlineContentFeatures;
+};
+
+
+
+/**
+ *
+ * @param {XMLDocument|string}
+ * @return {XMLDocument}
+ * @private
+ */
+dlfAltoParser.prototype.parseXML_ = function(document) {
+    if (typeof document === 'string' || document instanceof String) {
+        return $.parseXML(document);
+    };
+    return document;
+};
---
+++ Resources/Public/Javascript/PageView/OL3Sources.js
@@ -0,0 +1,352 @@
+/**
+ * (c) Kitodo. Key to digital objects e.V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo and TYPO3 projects.
+ *
+ * @license GNU General Public License version 3 or later.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+/**
+ * Namespace for dlfViewer ol3 sources
+ * @namespace
+ */
+var dlfViewerSource = dlfViewerSource || {};
+
+/**
+ * Utility Function based on Google Closure Library.
+ * http://google.github.io/closure-library/api/source/closure/goog/object/object.js.src.html#l306
+ *
+ * Searches an object for an element that satisfies the given condition and
+ * returns its key.
+ *
+ * @param {Object<K,V>} obj The object to search in.
+ * @param {function(this:T,V,string,Object<K,V>):boolean} f The
+ *      function to call for every element. Takes 3 arguments (the value,
+ *     the key and the object) and should return a boolean.
+ * @param {T=} opt_this An optional "this" context for the function.
+ * @return {string|undefined} The key of an element for which the function
+ *     returns true or undefined if no such element is found.
+ */
+dlfViewerSource.findKey = function(obj, f, opt_this) {
+    for (var key in obj) {
+        if (f.call(/** @type {?} */ (opt_this), obj[key], key, obj)) {
+            return key;
+        }
+    }
+    return undefined;
+};
+
+/**
+ * OpenLayers 3 TileLoadFunction based on the work of Klokan Technologies GmbH (http://www.klokantech.com) and
+ * the IIIFViewer. See: https://github.com/klokantech/iiifviewer/blob/master/src/iiifsource.js
+ *
+ * @param {number} tileSize
+ * @param {ol.ImageTile} tile
+ * @param {string} url
+ */
+dlfViewerSource.tileLoadFunction = function(tileSize, tile, url) {
+    var img = tile.getImage();
+    $(img).load(function() {
+        if (img.naturalWidth > 0 &&
+          (img.naturalWidth != tileSize || img.naturalHeight != tileSize)) {
+            var canvas = document.createElement('canvas');
+            canvas.width = tileSize;
+            canvas.height = tileSize;
+
+            var ctx = canvas.getContext('2d');
+            ctx.drawImage(img, 0, 0);
+
+            var key = dlfViewerSource.findKey(tile, function(v) {return v == img;});
+            if (key) {
+                tile[key] = canvas;
+            }
+        }
+    });
+    img.src = url;
+};
+
+/**
+ * OpenLayers 3 compatible source object for an iiif server.
+ *
+ * Based on the work of Klokan Technologies GmbH (http://www.klokantech.com) and the IIIFViewer. See:
+ * https://github.com/klokantech/iiifviewer/blob/master/src/iiifsource.js
+ *
+ * @param {Object} options
+ * @constructor
+ */
+dlfViewerSource.IIIF = function(options) {
+
+    // parse parameters
+    var url = options.url,
+        format = options.format !== undefined ? options.format : 'jpg',
+        quality = options.quality !== undefined ? options.quality : 'native',
+        width = options.size[0],
+        height = options.size[1],
+        tileSize = options.tileSize !== undefined ? options.tileSize : 256,
+        origin = options.crossOrigin !== undefined ? options.crossOrigin : '*',
+        offset = options.offset !==  undefined ? options.offset : [0, 0],
+        resolutions = $.extend([], options.resolutions),
+        projection = options.projection;
+
+    // sort resolutions because the spec does not specify any order
+    resolutions.sort(function(a, b) {
+        return a - b;
+    });
+    var ignoreScaleFactors = false,
+        maxZoom;
+
+    // check if provided resolutions are consecutive powers to 2
+    for (var i=0; i < resolutions.length; i++) {
+        if (Math.pow(2,i) != resolutions[i]) {
+            ignoreScaleFactors = true;
+            break;
+        }
+    }
+
+    // no resolutions are provided or they can't be continuously calculated with a zoomFactor of 2
+    if (resolutions.length == 0 || ignoreScaleFactors) {
+        maxZoom = Math.max(
+            Math.ceil( Math.log(width / tileSize) / Math.LN2),
+            Math.ceil( Math.log(height / tileSize) / Math.LN2)
+        );
+        // provide resolutions from 2^0 to 2^maxZoom
+        resolutions = [];
+        for (var i = 0; i <= maxZoom; i++) {
+            resolutions.push(Math.pow(2,i));
+        }
+    } else {
+        var maxScaleFactor = Math.max.apply(null, resolutions);
+        maxZoom = Math.round(Math.log(maxScaleFactor) / Math.LN2);
+    }
+
+    var tierSizes = [];
+    for (var i = 0; i <= maxZoom; i++) {
+        var scale = Math.pow(2, maxZoom - i);
+        var width_ = Math.ceil(width / scale);
+        var height_ = Math.ceil(height / scale);
+        var tilesX_ = Math.ceil(width_ / tileSize);
+        var tilesY_ = Math.ceil(height_ / tileSize);
+        tierSizes.push([tilesX_, tilesY_]);
+    }
+
+    // define tilegrid with offset extent
+    var extent = [offset[0], offset[1] - height, offset[0] + width, offset[1]];
+    var tileGrid = new ol.tilegrid.TileGrid({
+            extent: extent,
+            resolutions: resolutions.reverse(),
+            origin: ol.extent.getTopLeft(extent),
+            tileSize: tileSize
+        });
+
+    /**
+     * @this {ol.source.TileImage}
+     * @param {ol.TileCoord} tileCoord Tile Coordinate.
+     * @param {number} pixelRatio Pixel ratio.
+     * @param {ol.proj.Projection} projection Projection.
+     * @return {string|undefined} Tile URL.
+     */
+    var tileUrlFunction = function(tileCoord, pixelRatio, projection) {
+
+        var z = tileCoord[0];
+        if (maxZoom < z) {
+            return undefined;
+        }
+
+        var sizes = tierSizes[z];
+        if (!sizes) {
+            return undefined;
+        }
+
+        var x = tileCoord[1];
+        var y = -tileCoord[2] - 1;
+        if (x < 0 || sizes[0] <= x || y < 0 || sizes[1] <= y) {
+            return undefined;
+        } else {
+            var scale = Math.pow(2, maxZoom - z);
+            var tileBaseSize = tileSize * scale;
+            var minx = x * tileBaseSize;
+            var miny = y * tileBaseSize;
+            var maxx = Math.min(minx + tileBaseSize, width);
+            var maxy = Math.min(miny + tileBaseSize, height);
+
+            maxx = scale * Math.floor(maxx / scale);
+            maxy = scale * Math.floor(maxy / scale);
+
+            var query = '/' + minx + ',' + miny + ',' +
+              (maxx - minx) + ',' + (maxy - miny) +
+              '/pct:' + (100 / scale) + '/0/' + quality + '.' + format;
+
+            return url + query;
+        }
+    };
+
+    var tileImageParams = {
+        crossOrigin: origin,
+        projection: projection,
+        tileGrid: tileGrid,
+        tileUrlFunction: tileUrlFunction
+    };
+
+    if (ol.has.CANVAS) {
+        tileImageParams.tileLoadFunction = dlfViewerSource.tileLoadFunction.bind(this, tileSize);
+    }
+
+    return new ol.source.TileImage(tileImageParams);
+};
+
+/**
+ * Returns an iiif compatible metadata url.
+ *
+ * @param {string} baseUrl
+ * @return {string}
+ */
+dlfViewerSource.IIIF.getMetdadataURL = function(baseUrl) {
+    var pathString = baseUrl.lastIndexOf('/') + 1 === baseUrl.length
+        ? 'info.json'
+        : '/info.json';
+
+    return baseUrl + pathString;
+};
+
+/**
+ * OpenLayers 3 compatible source object for an iip server.
+ *
+ *
+ * @param {Object} options
+ * @constructor
+ */
+dlfViewerSource.IIP = function(options) {
+
+    // parse parameters
+    var url = options.url.indexOf('?') > -1
+          ? options.url
+          : options.url + '?',
+        width = options.size[0],
+        height = options.size[1],
+        tileSize = options.tileSize !== undefined ? options.tileSize : 256,
+        origin = options.crossOrigin !== undefined ? options.crossOrigin : '*',
+        offset = options.offset !==  undefined ? options.offset : [0, 0],
+        resolutions = [],
+        projection = options.projection;
+
+    // calculate tiersize in tiles and resolutions
+    var tierSizeInTiles = [],
+        imageWidth = width,
+        imageHeight = height,
+        res = 1;
+    while (imageWidth > tileSize || imageHeight > tileSize) {
+
+        tierSizeInTiles.push([
+            Math.ceil(imageWidth / tileSize),
+            Math.ceil(imageHeight / tileSize)
+        ]);
+        resolutions.push( res );
+
+        imageWidth >>= 1;
+        imageHeight >>= 1;
+        res += res;
+
+    };
+    resolutions.push( res );
+    tierSizeInTiles.push( [1,1]);
+    tierSizeInTiles.reverse();
+
+    var extent = [offset[0], offset[1] - height, offset[0] + width, offset[1]];
+    var tileGrid = new ol.tilegrid.TileGrid({
+        extent: extent,
+        resolutions: resolutions.reverse(),
+        origin: ol.extent.getTopLeft(extent),
+        tileSize: tileSize
+    });
+
+    /**
+     * @this {ol.source.TileImage}
+     * @param {ol.TileCoord} tileCoord Tile Coordinate.
+     * @param {number} pixelRatio Pixel ratio.
+     * @param {ol.proj.Projection} projection Projection.
+     * @return {string|undefined} Tile URL.
+     */
+    var tileUrlFunction = function(tileCoord, pixelRatio, projection) {
+        if (tileCoord === undefined ||tileCoord === null) {
+            return undefined;
+        } else {
+            var resolution = tileCoord[0],
+                tileCoordX = tileCoord[1],
+                tileCoordY = -tileCoord[2] - 1,
+                tileIndex = tileCoordY * tierSizeInTiles[resolution][0] + tileCoordX;
+            return url + '&JTL=' + resolution + ',' + tileIndex;
+        }
+    };
+
+    var tileImageParams = {
+        crossOrigin: origin,
+        projection: projection,
+        tileGrid: tileGrid,
+        tileUrlFunction: tileUrlFunction
+    };
+
+    if (ol.has.CANVAS) {
+        tileImageParams.tileLoadFunction = dlfViewerSource.tileLoadFunction.bind(this, tileSize);
+    }
+
+    return new ol.source.TileImage(tileImageParams);
+
+
+};
+
+/**
+ * Returns an iip compatible metadata url.
+ *
+ * @param {string} baseUrl
+ * @return {string}
+ */
+dlfViewerSource.IIP.getMetdadataURL = function(baseUrl) {
+    var queryString = '&obj=IIP,1.0&obj=Max-size&obj=Tile-size&obj=Resolution-number',
+        url = baseUrl.indexOf('?') > -1
+          ? baseUrl
+          : baseUrl + '?';
+    return url + queryString;
+};
+
+/**
+ * Function parses a given string response for a iip metadata request.
+ *
+ * @param {string} metadataString
+ * @return {Object}
+ */
+dlfViewerSource.IIP.parseMetadata = function(metadataString) {
+
+    // parse size
+    var maxSize = metadataString.split( "Max-size" );
+    if(!maxSize[1]) {
+        return null;
+    }
+    var size = maxSize[1].split(" ");
+
+    // parse tile size
+    var tileSizeTmp = metadataString.split( "Tile-size" );
+    if(!tileSizeTmp[1]) {
+        return null;
+    }
+    var tileSize = tileSizeTmp[1].split(" ");
+
+    // parse resolution
+    var resolutionNumTmp = metadataString.split( "Resolution-number"),
+        resolutionNum = parseInt( resolutionNumTmp[1].substring(1,resolutionNumTmp[1].length)),
+        res = 1,
+        resolutions = [];
+    for (var i = 0; i <resolutionNum; i++) {
+        resolutions.push(res);
+        res += res;
+    }
+
+    var metadataObj = {
+        'width': parseInt(size[0].substring(1,size[0].length)),
+        'height': parseInt(size[1]),
+        'tilesize': [parseInt(tileSize[0].substring(1,tileSize[0].length)), parseInt(tileSize[1]) ],
+        'resolutions': resolutions
+    };
+    return metadataObj;
+};
