fed9a0f3bee37919a5ca10d4d3c957fc6909c67d
Alexander Bigga <alexander.bigga@slub-dresden.de>
2019-09-06 12:22:12 +0200

    Use proper COUNT statement in SQL
    The current solution in master does not work because the MYSQL $result
    is empty after fetchAll(). You cannot execute a loop with
    $result->fetch() afterwards.
    
    The PDOStatement::rowCount() would fix this issue at least for MySQL and
    MariaDB but the documentation warns to use it for SELECT queries.
    
    The suggested solution is to make a proper COUNT statement. See the
    following issue and patch as example:
    
    https://forge.typo3.org/issues/78129

--- Classes/Plugin/Collection.php
+++ Classes/Plugin/Collection.php
@@ -104,7 +104,7 @@ class Collection extends \Kitodo\Dlf\Common\AbstractPlugin {
         }
 
         // Get collections.
-        $result = $queryBuilder
+        $queryBuilder
             ->select(
                 'tx_dlf_collections.index_name AS index_name',
                 'tx_dlf_collections.index_search as index_query',
@@ -129,12 +129,10 @@ class Collection extends \Kitodo\Dlf\Common\AbstractPlugin {
                 ),
                 Helper::whereExpression('tx_dlf_collections')
             )
-            ->orderBy($orderBy)
-            ->execute();
+            ->orderBy($orderBy);
 
-        $allResults = $result->fetchAll();
-
-        $count = count($allResults);
+        $result = $queryBuilder->execute();
+        $count = $queryBuilder->count('uid')->execute()->fetchColumn(0);
         $content = '';
         if ($count == 1 && empty($this->conf['dont_show_single'])) {
             $resArray = $allResults[0];
