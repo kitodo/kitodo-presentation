808da3194ca2b8232f4091833cebe994dc3deadf
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2019-10-01 11:07:08 +0200

    Fix code style

--- Classes/Common/AbstractPlugin.php
+++ Classes/Common/AbstractPlugin.php
@@ -64,18 +64,16 @@ abstract class AbstractPlugin extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugin 
      *
      * @return void
      */
-    protected function getTemplate($part = '###TEMPLATE###', $directory = '', $tool = false) {
+    protected function getTemplate($part = '###TEMPLATE###', $directory = '', $tool = FALSE) {
         $this->templateService = GeneralUtility::makeInstance(MarkerBasedTemplateService::class);
-
         if (!empty($this->conf['templateFile'])) {
             // Load template file from configuration.
             if ($tool) {
-                $templateLocation = 'EXT:'.$this->extKey.'/Resources/Private/Templates/' . Helper::getUnqualifiedClassName(get_class($this)) . '.tmpl';
+                $templateLocation = 'EXT:'.$this->extKey.'/Resources/Private/Templates/'.Helper::getUnqualifiedClassName(get_class($this)).'.tmpl';
                 $this->template = $this->templateService->getSubpart($this->cObj->fileResource($templateLocation), $part);
             } else {
                 $this->template = $this->templateService->getSubpart($this->cObj->fileResource($this->conf['templateFile']), $part);
             }
-
         } else {
             if (empty($directory)) {
                 $templateLocation = 'EXT:'.$this->extKey.'/Resources/Private/Templates/' . Helper::getUnqualifiedClassName(get_class($this)) . '.tmpl';
@@ -86,7 +84,6 @@ abstract class AbstractPlugin extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugin 
             // Load default template file.
             $this->template = $this->templateService->getSubpart($this->cObj->fileResource($templateLocation), $part);
         }
-
     }
 
     /**
