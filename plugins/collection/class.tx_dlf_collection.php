bb51601bb6ffdd2cc1fbd06a1014fac49d9f4b07
Alexander Bigga <alexander.bigga@slub-dresden.de>
2019-11-13 09:29:49 +0100

    fix line indent

--- plugins/collection/class.tx_dlf_collection.php
+++ plugins/collection/class.tx_dlf_collection.php
@@ -491,7 +491,7 @@ class tx_dlf_collection extends tx_dlf_plugin {
                    'h' => '',
                    's' => $sorting,
                    'p' => array ()
-               );
+                );
 
             }
 
