cf12bfe84da639dad360f28e4f010c84ffde7a3f
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2019-11-15 12:56:44 +0100

    Fix code style

--- Classes/Plugin/PageView.php
+++ Classes/Plugin/PageView.php
@@ -119,14 +119,14 @@ class PageView extends \Kitodo\Dlf\Common\AbstractPlugin
         $markerArray = [];
         if ($this->piVars['id']) {
             if ($this->conf['crop']) {
-                $markerArray['###EDITBUTTON###'] = '<a href="javascript: tx_dlf_viewer.activateSelection();" title="' . $this->pi_getLL('editMode', '', TRUE) . '">' . $this->pi_getLL('editMode', '', TRUE) . '</a>';
-                $markerArray['###EDITREMOVE###'] = '<a href="javascript: tx_dlf_viewer.resetCropSelection();" title="' . $this->pi_getLL('editRemove', '', TRUE) . '">' . $this->pi_getLL('editRemove', '', TRUE) . '</a>';
+                $markerArray['###EDITBUTTON###'] = '<a href="javascript: tx_dlf_viewer.activateSelection();" title="' . $this->pi_getLL('editMode', '', true) . '">' . $this->pi_getLL('editMode', '', true) . '</a>';
+                $markerArray['###EDITREMOVE###'] = '<a href="javascript: tx_dlf_viewer.resetCropSelection();" title="' . $this->pi_getLL('editRemove', '', true) . '">' . $this->pi_getLL('editRemove', '', true) . '</a>';
             } else {
                 $markerArray['###EDITBUTTON###'] = '';
                 $markerArray['###EDITREMOVE###'] = '';
             }
             if ($this->conf['magnifier']) {
-                $markerArray['###MAGNIFIER###'] = '<a href="javascript: tx_dlf_viewer.activateMagnifier();" title="' . $this->pi_getLL('magnifier', '', TRUE) . '">' . $this->pi_getLL('magnifier', '', true) . '</a>';
+                $markerArray['###MAGNIFIER###'] = '<a href="javascript: tx_dlf_viewer.activateMagnifier();" title="' . $this->pi_getLL('magnifier', '', true) . '">' . $this->pi_getLL('magnifier', '', true) . '</a>';
             } else {
                 $markerArray['###MAGNIFIER###'] = '';
             }
