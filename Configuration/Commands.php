e7a49bddcf13e8050ed52dd6e89e19afcc84338a
Sebastian Meyer <sebastian.meyer@slub-dresden.de>
2019-11-15 07:55:58 +0100

    Fix intendation

--- Configuration/Commands.php
+++ Configuration/Commands.php
@@ -10,7 +10,7 @@
  * LICENSE.txt file that was distributed with this source code.
  */
 
- /**
+/**
  * Commands to be executed by TYPO3, where the key of the array
  * is the name of the command (to be called as the first argument after "typo3").
  * Required parameter is the "class" of the command which needs to be a subclass
