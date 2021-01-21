687c5cbbe3b9643e02d529eba1b255f00ceb1b71
Alexander Bigga <alexander.bigga@slub-dresden.de>
2021-01-21 16:33:42 +0100

    Add Solr core name on calculation the cache identifier.
    As the Solr core name is not part of the parameters array and the
    $this->params is empty anyhow. The cacheidentifier of same queries is
    identical over different Solr cores.
    
    This just adds the core name to the identifier calculation.

--- Classes/Common/Solr.php
+++ Classes/Common/Solr.php
@@ -428,7 +428,7 @@ class Solr
         $parameters['query'] = $query;
 
         // calculate cache identifier
-        $cacheIdentifier = hash('md5', print_r(array_merge($this->params, $parameters), 1));
+        $cacheIdentifier = hash('md5', $this->core . print_r(array_merge($this->params, $parameters), 1));
         $cache = GeneralUtility::makeInstance(CacheManager::class)->getCache('tx_dlf_solr');
 
         $resultSet = [];
