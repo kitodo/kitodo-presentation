ca977c0714bf6184e4cc3268be5ca332717415a1
Beatrycze Volk <beatrycze.volk@slub-dresden.de>
2021-02-02 15:29:08 +0100

    Move search to own method

--- Resources/Public/Javascript/PageView/SearchInDocument.js
+++ Resources/Public/Javascript/PageView/SearchInDocument.js
@@ -8,12 +8,12 @@
  * LICENSE.txt file that was distributed with this source code.
  */
 
- /**
-  * This function increases the start parameter of the search form and submits
-  * the form.
-  *
-  * @returns void
-  */
+/**
+ * This function increases the start parameter of the search form and submits
+ * the form.
+ *
+ * @returns void
+ */
 function nextResultPage() {
     var currentStart = $("#tx-dlf-search-in-document-form input[id='tx-dlf-search-in-document-start']").val();
     var newStart = parseInt(currentStart) + 20;
@@ -50,9 +50,9 @@ function resetStart() {
  * @returns void
  */
 function addHighlightEffect(highlightIds) {
-    highlightIds.forEach(function(highlightId) {
+    highlightIds.forEach(function (highlightId) {
         var targetElement = $('#' + highlightId);
-        
+
         if (targetElement.length > 0 && !targetElement.hasClass('highlight')) {
             targetElement.addClass('highlight');
         }
@@ -80,88 +80,86 @@ function getBaseUrl() {
     return baseUrl;
 }
 
-$(document).ready(function() {
-    alert("Document is ready...");
-    $("#tx-dlf-search-in-document-button").click(function() {
-        resetStart();
+function search() {
+    resetStart();
 
-        alert("Handler for .click() called...");
+    $('#tx-dlf-search-in-document-loading').show();
+    $('#tx-dlf-search-in-document-clearing').hide();
+    $('#tx-dlf-search-in-document-button-next').hide();
+    $('#tx-dlf-search-in-document-button-previous').hide();
+    // Send the data using post
+    $.post(
+        "/",
+        {
+            eID: "tx_dlf_search_in_document",
+            q: $( "input[id='tx-dlf-search-in-document-query']" ).val(),
+            uid: $( "input[id='tx-dlf-search-in-document-id']" ).val(),
+            start: $( "input[id='tx-dlf-search-in-document-start']" ).val(),
+            encrypted: $( "input[id='tx-dlf-search-in-document-encrypted']" ).val(),
+        },
+        function(data) {
+            var resultItems = [];
+            var resultList = '<div class="results-active-indicator"></div><ul>';
+            var start = -1;
+            if (data['numFound'] > 0) {
+                data['documents'].forEach(function (element, i) {
+                    if (start < 0) {
+                        start = i;
+                    }
+                    var searchWord = element['snippet'];
+                    searchWord = searchWord.substring(searchWord.indexOf('<em>') + 4, searchWord.indexOf('</em>'));
 
-        $('#tx-dlf-search-in-document-loading').show();
-        $('#tx-dlf-search-in-document-clearing').hide();
-        $('#tx-dlf-search-in-document-button-next').hide();
-        $('#tx-dlf-search-in-document-button-previous').hide();
-        // Send the data using post
-        $.post(
-            "/",
-            {
-                eID: "tx_dlf_search_in_document",
-                q: $( "input[id='tx-dlf-search-in-document-query']" ).val(),
-                uid: $( "input[id='tx-dlf-search-in-document-id']" ).val(),
-                start: $( "input[id='tx-dlf-search-in-document-start']" ).val(),
-                encrypted: $( "input[id='tx-dlf-search-in-document-encrypted']" ).val(),
-            },
-            function(data) {
-                console.log(data);
-                var resultItems = [];
-                var resultList = '<div class="results-active-indicator"></div><ul>';
-                var start = -1;
-                if (data['numFound'] > 0) {
-                    data['documents'].forEach(function (element, i) {
-                        if (start < 0) {
-                            start = i;
-                        }
-                        var searchWord = element['snippet'];
-                        searchWord = searchWord.substring(searchWord.indexOf('<em>') + 4, searchWord.indexOf('</em>'));
+                    var link = getBaseUrl()
+                        + 'tx_dlf[id]=' + element['uid']
+                        + '&tx_dlf[highlight_word]=' + encodeURIComponent(searchWord)
+                        + '&tx_dlf[page]=' + element['page'];
 
-                        var link = getBaseUrl()
-                            + 'tx_dlf[id]=' + element['uid']
-                            + '&tx_dlf[highlight_word]=' + encodeURIComponent(searchWord)
-                            + '&tx_dlf[page]=' + element['page'];
+                    if (element['snippet'].length > 0) {
+                        resultItems[element['page']] = '<span class="structure">'
+                            + $('#tx-dlf-search-in-document-label-page').text() + ' ' + element['page']
+                            + '</span><br />'
+                            + '<span ="textsnippet">'
+                            + '<a href=\"' + link + '\">' + element['snippet'] + '</a>'
+                            + '</span>';
+                    }
 
-                        if (element['snippet'].length > 0) {
-                            resultItems[element['page']] = '<span class="structure">'
-                                + $('#tx-dlf-search-in-document-label-page').text() + ' ' + element['page']
-                                + '</span><br />'
-                                + '<span ="textsnippet">'
-                                + '<a href=\"' + link + '\">' + element['snippet'] + '</a>'
-                                + '</span>';
-                        }
-
-                        // TODO: highlight found phrase in full text - verify page?
-                        if (element['highlight'].length > 0) {
-                            addHighlightEffect(element['highlight']);
-                        }
-                        // TODO: highlight found phrase in image
-                    });
-                    // Sort result by page.
-                    resultItems.sort(function (a, b) {
-                        return a - b;
-                    });
-                    resultItems.forEach(function (item, index) {
-                        resultList += '<li>' + item + '</li>';
-                    });
-                } else {
-                    resultList += '<li class="noresult">' + $('#tx-dlf-search-in-document-label-noresult').text() + '</li>';
-                }
-                resultList += '</ul>';
-                if (start > 0) {
-                    resultList += '<input type="button" id="tx-dlf-search-in-document-button-previous" class="button-previous" onclick="previousResultPage();" value="' + $('#tx-dlf-search-in-document-label-previous').text() + '" />';
-                }
-                if (data['numFound'] > (start + 20)) {
-                    resultList += '<input type="button" id="tx-dlf-search-in-document-button-next" class="button-next" onclick="nextResultPage();" value="' + $('#tx-dlf-search-in-document-label-next').text() + '" />';
-                }
-                $('#tx-dlf-search-in-document-results').html(resultList);
-            },
-            "json"
-        )
-        .done(function( data ) {
+                    // TODO: highlight found phrase in full text - verify page?
+                    if (element['highlight'].length > 0) {
+                        addHighlightEffect(element['highlight']);
+                    }
+                    // TODO: highlight found phrase in image
+                });
+                // Sort result by page.
+                resultItems.sort(function (a, b) {
+                    return a - b;
+                });
+                resultItems.forEach(function (item, index) {
+                    resultList += '<li>' + item + '</li>';
+                });
+            } else {
+                resultList += '<li class="noresult">' + $('#tx-dlf-search-in-document-label-noresult').text() + '</li>';
+            }
+            resultList += '</ul>';
+            if (start > 0) {
+                resultList += '<input type="button" id="tx-dlf-search-in-document-button-previous" class="button-previous" onclick="previousResultPage();" value="' + $('#tx-dlf-search-in-document-label-previous').text() + '" />';
+            }
+            if (data['numFound'] > (start + 20)) {
+                resultList += '<input type="button" id="tx-dlf-search-in-document-button-next" class="button-next" onclick="nextResultPage();" value="' + $('#tx-dlf-search-in-document-label-next').text() + '" />';
+            }
+            $('#tx-dlf-search-in-document-results').html(resultList);
+        },
+        "json"
+    )
+        .done(function (data) {
             $('#tx-dfgviewer-sru-results-loading').hide();
             $('#tx-dfgviewer-sru-results-clearing').show();
         });
-    });
-      // clearing button
-    $('#tx-dlf-search-in-document-clearing').click(function() {
+}
+
+$(document).ready(function () {
+    alert("Document is ready...");
+    // clearing button
+    $('#tx-dlf-search-in-document-clearing').click(function () {
         $('#tx-dlf-search-in-document-results ul').remove();
         $('.results-active-indicator').remove();
         $('#tx-dlf-search-in-document-query').val('');
