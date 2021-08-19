9d4ec6a80d468319ab29974688ab4dd8e082ba74
Christopher Timm <timm@effective-webwork.de>
2021-08-19 10:22:52 +0200

    Add navigation controller and templates

--- ext_localconf.php
+++ ext_localconf.php
@@ -242,3 +242,15 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][] = [
     'priority' => 30,
     'class' => \Kitodo\Dlf\Hooks\Form\FieldInformation\SolrCoreStatus::class
 ];
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'Kitodo.Dlf',
+    'Navigation',
+    [
+        Navigation::class => 'main',
+    ],
+    // non-cacheable actions
+    [
+        Navigation::class => 'main',
+    ]
+);
