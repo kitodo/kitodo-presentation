33f40f9d4c1e5ad1dc648b5e832b82074f451a3f
haogatyp <gatermann@effective-webwork.de>
2021-08-20 16:29:58 +0200

    Listview plugin to extbase/fluid.

--- ext_localconf.php
+++ ext_localconf.php
@@ -242,3 +242,26 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][] = [
     'priority' => 30,
     'class' => \Kitodo\Dlf\Hooks\Form\FieldInformation\SolrCoreStatus::class
 ];
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'Kitodo.Dlf',
+    'ListView',
+    [
+        ListView::class => 'main',
+    ],
+    // non-cacheable actions
+    [
+        ListView::class => 'main',
+    ]
+);
+
+
+
+
+
+
+
+
+
+
+
