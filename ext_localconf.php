a54c24a81b20d30e8ec3e1da0ba2b2ecf6524325
haogatyp <gatermann@effective-webwork.de>
2021-08-27 10:48:03 +0200

    AudioPlayer plugin to extbase/fluid.

--- ext_localconf.php
+++ ext_localconf.php
@@ -242,3 +242,18 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][] = [
     'priority' => 30,
     'class' => \Kitodo\Dlf\Hooks\Form\FieldInformation\SolrCoreStatus::class
 ];
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'Kitodo.Dlf',
+    'AudioPlayer',
+    [
+        AudioPlayer::class => 'main',
+    ],
+    // non-cacheable actions
+    [
+        AudioPlayer::class => 'main',
+    ]
+);
+
+
+
