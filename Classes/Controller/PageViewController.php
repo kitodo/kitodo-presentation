069203438ff30e6130dd746d02da432db7739daa
Alexander Bigga <alexander.bigga@slub-dresden.de>
2021-10-25 11:12:26 +0200

    Cleanup pageview controller

--- Classes/Controller/PageViewController.php
+++ Classes/Controller/PageViewController.php
@@ -11,36 +11,15 @@
 
 namespace Kitodo\Dlf\Controller;
 
-use \TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\MathUtility;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
-use Kitodo\Dlf\Common\Document;
-use Kitodo\Dlf\Common\DocumentList;
-use Kitodo\Dlf\Common\Helper;
-use Kitodo\Dlf\Common\Indexer;
-use Kitodo\Dlf\Common\Solr;
 use TYPO3\CMS\Core\Database\Connection;
-use TYPO3\CMS\Core\Database\ConnectionPool;
-use \Kitodo\Dlf\Domain\Model\SearchForm;
 use Ubl\Iiif\Presentation\Common\Model\Resources\ManifestInterface;
 use Ubl\Iiif\Presentation\Common\Vocabulary\Motivation;
 
-class PageViewController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+class PageViewController extends AbstractController
 {
-    public $prefixId = 'tx_dlf';
-    public $extKey = 'dlf';
-
-    /**
-     * @var ConfigurationManager
-     */
-    protected $configurationManager;
-
-    /**
-     * @var \TYPO3\CMS\Core\Log\LogManager
-     */
-    protected $logger;
-
     /**
      * @var
      */
@@ -55,79 +34,16 @@ class PageViewController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControl
     protected $controls = [];
 
     /**
-     * SearchController constructor.
-     * @param $configurationManager
-     */
-    public function __construct(ConfigurationManager $configurationManager)
-    {
-        $this->configurationManager = $configurationManager;
-        $this->logger = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Core\Log\LogManager')->getLogger(__CLASS__);
-    }
-
-    // TODO: Needs to be placed in an abstract class
-    /**
-     * Loads the current document into $this->doc
-     *
-     * @access protected
+     * The main method of the plugin
      *
      * @return void
      */
-    protected function loadDocument($requestData)
-    {
-        // Check for required variable.
-        if (
-            !empty($requestData['id'])
-            && !empty($this->settings['pages'])
-        ) {
-            // Should we exclude documents from other pages than $this->settings['pages']?
-            $pid = (!empty($this->settings['excludeOther']) ? intval($this->settings['pages']) : 0);
-            // Get instance of \Kitodo\Dlf\Common\Document.
-            $this->doc = Document::getInstance($requestData['id'], $pid);
-            if (!$this->doc->ready) {
-                // Destroy the incomplete object.
-                $this->doc = null;
-                $this->logger->error('Failed to load document with UID ' . $requestData['id']);
-            } else {
-                // Set configuration PID.
-                $this->doc->cPid = $this->settings['pages'];
-            }
-        } elseif (!empty($requestData['recordId'])) {
-            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
-                ->getQueryBuilderForTable('tx_dlf_documents');
-
-            // Get UID of document with given record identifier.
-            $result = $queryBuilder
-                ->select('tx_dlf_documents.uid AS uid')
-                ->from('tx_dlf_documents')
-                ->where(
-                    $queryBuilder->expr()->eq('tx_dlf_documents.record_id', $queryBuilder->expr()->literal($requestData['recordId'])),
-                    Helper::whereExpression('tx_dlf_documents')
-                )
-                ->setMaxResults(1)
-                ->execute();
-
-            if ($resArray = $result->fetch()) {
-                $requestData['id'] = $resArray['uid'];
-                // Set superglobal $_GET array and unset variables to avoid infinite looping.
-                $_GET[$this->prefixId]['id'] = $requestData['id'];
-                unset($requestData['recordId'], $_GET[$this->prefixId]['recordId']);
-                // Try to load document.
-                $this->loadDocument();
-            } else {
-                $this->logger->error('Failed to load document with record ID "' . $requestData['recordId'] . '"');
-            }
-        } else {
-            $this->logger->error('Invalid UID ' . $requestData['id'] . ' or PID ' . $this->settings['pages'] . ' for document loading');
-        }
-    }
-
-
     public function mainAction()
     {
         $requestData = GeneralUtility::_GPmerged('tx_dlf');
         unset($requestData['__referrer'], $requestData['__trustedProperties']);
 
-        $this->extConf = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get($this->extKey);
+        $this->extConf = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('dlf');
 
         // Load current document.
         $this->loadDocument($requestData);
@@ -136,7 +52,7 @@ class PageViewController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControl
             || $this->doc->numPages < 1
         ) {
             // Quit without doing anything if required variables are not set.
-            return '';
+            return;
         } else {
             if (!empty($requestData['logicalPage'])) {
                 $requestData['page'] = $this->doc->getPhysicalPage($requestData['logicalPage']);
@@ -335,5 +251,4 @@ class PageViewController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControl
         }
         return $image;
     }
-
 }
