058fa603af07efe0d28abdbd258dcd7dfe79764b
Christopher Timm <timm@effective-webwork.de>
2021-11-25 11:22:40 +0100

    Check typo3 version and do the bootstrapping only if version less than 10

--- Classes/Hooks/ConfigurationForm.php
+++ Classes/Hooks/ConfigurationForm.php
@@ -19,6 +19,7 @@ use TYPO3\CMS\Core\Core\Bootstrap;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 
 /**
  * Hooks and helper for \TYPO3\CMS\Core\TypoScript\ConfigurationForm
@@ -73,6 +74,7 @@ class ConfigurationForm
      */
     public function checkMetadataFormats()
     {
+        if (explode('.',VersionNumberUtility::getCurrentTypo3Version())[0] < 10) {
         // We need to do some bootstrapping manually as of TYPO3 9.
         // Load table configuration array into $GLOBALS['TCA'].
         ExtensionManagementUtility::loadBaseTca(false);
@@ -82,6 +84,7 @@ class ConfigurationForm
         Bootstrap::initializeBackendUser();
         // Initialize backend and ensure authenticated access.
         Bootstrap::initializeBackendAuthentication();
+        }
 
         $nsDefined = [
             'MODS' => false,
