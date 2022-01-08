be7a39b634a166ecaffc8c549783aac263319b28
Alexander Bigga <alexander.bigga@slub-dresden.de>
2022-01-08 18:01:29 +0100

    Use default owner only if no other owner is found

--- Classes/Common/Document.php
+++ Classes/Common/Document.php
@@ -1130,7 +1130,7 @@ abstract class Document
 
         // Get UID for owner.
         if (empty($owner)) {
-            $owner = empty($metadata['owner'][0]) ? $metadata['owner'][0] : 'default';
+            $owner = $metadata['owner'][0] ? : 'default';
         }
         if (!MathUtility::canBeInterpretedAsInteger($owner)) {
             $result = $queryBuilder
