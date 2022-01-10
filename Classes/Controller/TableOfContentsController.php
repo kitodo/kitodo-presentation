c55a805ef5d99f94491cd334d3122a03af551317
Alexander Bigga <alexander.bigga@slub-dresden.de>
2022-01-10 13:53:45 +0100

    Better use parent::_construct()

--- Classes/Controller/TableOfContentsController.php
+++ Classes/Controller/TableOfContentsController.php
@@ -43,10 +43,10 @@ class TableOfContentsController extends AbstractController
      */
     public function __construct()
     {
+        parent::__construct();
         // Read plugin TS configuration.
         // TODO: This is more or less obsolete - the table of document plugin does not use the TypoScript anymore
         $this->pluginConf = $GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_dlf_tableofcontents.'];
-        $this->initialize();
     }
 
     /**
