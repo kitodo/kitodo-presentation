936cd4bc1c3564839fc32c810280f7ef7fa47bbf
Alexander Bigga <alexander.bigga@slub-dresden.de>
2022-01-31 23:08:01 +0100

    The paginate widget does not append queryString with POST as of TYPO3 10.4

--- ext_localconf.php
+++ ext_localconf.php
@@ -128,7 +128,7 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update'][\Kitodo\Dlf\U
     ],
     // non-cacheable actions
     [
-        Search::class => 'search',
+        Search::class => 'main, search',
     ]
 );
 
