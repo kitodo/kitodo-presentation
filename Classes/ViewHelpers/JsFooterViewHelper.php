e801dea61029f90848d28bc3c7ebfede91fb62c4
Alexander Bigga <alexander.bigga@slub-dresden.de>
2022-03-21 17:51:39 +0100

    Place the viewConfiguration to JsFooterInline

---
+++ Classes/ViewHelpers/JsFooterViewHelper.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * (c) Kitodo. Key to digital objects e.V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo and TYPO3 projects.
+ *
+ * @license GNU General Public License version 3 or later.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+namespace Kitodo\Dlf\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3\CMS\Core\Page\PageRenderer;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Add inline JavaScript code to footer *
+ */
+class JsFooterViewHelper extends AbstractViewHelper
+{
+    /**
+     * Initialize arguments.
+     */
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('inlineCode', 'string', 'Inline JavaScript', true);
+    }
+
+   /**
+     *
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     */
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ) {
+        $inlineCode = $arguments['inlineCode'];
+
+        /** @var $pageRenderer PageRenderer */
+        $pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
+        $pageRenderer->addJsFooterInlineCode('js-dlf-inline-footer', $inlineCode);
+    }
+}
