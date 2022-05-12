65053e769db3875c406fc5fcdd1fb98ab6980dff
Kajetan Dvoracek <kajetan.dvoracek@slub-dresden.de>
2022-05-13 01:42:53 +0200

    Unify handling of placeholders

--- Resources/Private/JavaScript/DlfMediaPlayer/index.js
+++ Resources/Private/JavaScript/DlfMediaPlayer/index.js
@@ -3,7 +3,7 @@
 export { default as Chapters } from './Chapters';
 export { ControlPanelButton, FullScreenButton, OverflowMenuButton } from './controls';
 export { action } from './lib/action';
-export { default as buildTimeString, timeStringFromTemplate } from './lib/buildTimeString';
+export { default as buildTimeString, getTimeStringPlaceholders } from './lib/buildTimeString';
 export { default as DlfMediaPlayer } from './DlfMediaPlayer';
 
 import DlfMediaPlayer from './DlfMediaPlayer';
