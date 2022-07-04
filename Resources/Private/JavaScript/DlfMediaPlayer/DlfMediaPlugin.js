b9e71ad56fe4cab007fb75ab08fd0982e2c26939
Kajetan Dvoracek <kajetan.dvoracek@slub-dresden.de>
2022-07-04 12:48:21 +0200

    Start implementing marker/segment edit and list
    - Keybindings to add markers and segments
    - `DlfMediaPlugin` for custom components that use a player
    - `MarkerTable` for listing, editing, exporting markers
    - Some documentation

---
+++ Resources/Private/JavaScript/DlfMediaPlayer/DlfMediaPlugin.js
@@ -0,0 +1,71 @@
+// @ts-check
+
+import Environment from '../lib/Environment';
+import DlfMediaPlayer from './DlfMediaPlayer';
+
+export default class DlfMediaPlugin extends HTMLElement {
+  constructor() {
+    super();
+
+    /** @protected @type {Translator & Identifier & Browser} */
+    this.env = new Environment();
+
+    /** @protected @type {DlfMediaPlayer | null} */
+    this.player = null;
+
+    /** @protected */
+    this.hasAttached = false;
+  }
+
+  get forPlayer() {
+    return this.getAttribute('forPlayer');
+  }
+
+  set forPlayer(value) {
+    if (value === null) {
+      this.removeAttribute('forPlayer');
+    } else {
+      this.setAttribute('forPlayer', value);
+    }
+  }
+
+  connectedCallback() {
+    if (!this.isConnected) {
+      return;
+    }
+
+    if (!this.hasAttached) {
+      // Wait for DOM being parsed
+      setTimeout(() => {
+        const forPlayer = this.forPlayer;
+        if (forPlayer === null) {
+          return;
+        }
+
+        const player = document.getElementById(forPlayer);
+        if (!(player instanceof DlfMediaPlayer)) {
+          return;
+        }
+
+        this.env = player.getEnv();
+        this.player = player;
+        this.attachToPlayer(player);
+      });
+
+      // Protect against unbounded loop ("alwaysPrependBottomControl" in WaveForm)
+      // TODO: Find a better solution
+      this.hasAttached = true;
+    }
+  }
+
+  /**
+   * Intended to be overridden in child class.
+   *
+   * @param {DlfMediaPlayer} player
+   * @protected
+   * @abstract
+   */
+  attachToPlayer(player) {
+    //
+  }
+}
