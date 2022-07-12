13e26036ff7ec0ce1539beb5443b1c6028b37ada
Kajetan Dvoracek <kajetan.dvoracek@slub-dresden.de>
2022-07-12 16:33:39 +0200

    Media Player: Add playrate slider for fine control

--- Resources/Private/JavaScript/DlfMediaPlayer/controls/index.js
+++ Resources/Private/JavaScript/DlfMediaPlayer/controls/index.js
@@ -4,5 +4,6 @@ export { default as ControlPanelButton } from './ControlPanelButton';
 export { default as FlatSeekBar } from './FlatSeekBar';
 export { default as FullScreenButton } from './FullScreenButton';
 export { default as OverflowMenuButton } from './OverflowMenuButton';
+export { default as PlaybackRateSelection } from './PlaybackRateSelection';
 export { default as PresentationTimeTracker } from './PresentationTimeTracker';
 export { default as VideoTrackSelection } from './VideoTrackSelection';
