7f2cb231c72c79c83291a324bbc67bb11fe6693f
Beatrycze Volk <beatrycze.volk@slub-dresden.de>
2022-07-19 12:29:57 +0200

    Add VIAF API classes

---
+++ Classes/Api/Viaf/ViafProfile.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * (c) Kitodo. Key to digital objects e.V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo and TYPO3 projects.
+ *
+ * @license GNU General Public License version 3 or later.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+namespace Kitodo\Dlf\Api\Viaf;
+
+use Kitodo\Dlf\Common\Helper;
+use TYPO3\CMS\Core\Http\RequestFactory;
+use TYPO3\CMS\Core\Log\LogManager;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * VIAF API Profile class
+ *
+ * @author Beatrycze Volk <beatrycze.volk@slub-dresden.de>
+ * @package TYPO3
+ * @subpackage dlf
+ * @access public
+ **/
+class ViafProfile
+{
+    /**
+     * This holds the logger
+     *
+     * @var LogManager
+     * @access protected
+     */
+    protected $logger;
+
+    /**
+     * This holds the client
+     *
+     * @var ViafClient
+     * @access protected
+     */
+    protected $client;
+
+    /**
+     * The raw VIAF profile
+     *
+     * @var \SimpleXmlElement
+     **/
+    private $raw = null;
+
+    /**
+     * Constructs client instance
+     *
+     * @param string $viafUrl: the VIAF URL of the profile
+     *
+     * @return void
+     **/
+    public function __construct($viafUrl)
+    {
+        $this->logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(static::class);
+        $this->client = new ViafClient($viafUrl, GeneralUtility::makeInstance(RequestFactory::class));
+    }
+
+    /**
+     * Get the VIAF profile data
+     *
+     * @return  object
+     **/
+    public function getData()
+    {
+        $this->getRaw();
+        if (!empty($this->raw)) {
+            $data = [];
+            $data['address'] = $this->getAddress();
+            $data['fullName'] = $this->getFullName();
+            return $data;
+        } else {
+            $this->logger->warning('No data found for given VIAF URL');
+            return false;
+        }
+    }
+
+    /**
+     * Get the address
+     *
+     * @return  string
+     **/
+    public function getAddress()
+    {
+        $this->getRaw();
+        if (!empty($this->raw->asXML())) {
+            return (string) $this->raw->xpath('./ns1:nationalityOfEntity/ns1:data/ns1:text')[0];
+        } else {
+            $this->logger->warning('No address found for given VIAF URL');
+            return false;
+        }
+    }
+
+    /**
+     * Get the full name
+     *
+     * @return  string
+     **/
+    public function getFullName()
+    {
+        $this->getRaw();
+        if (!empty($this->raw->asXML())) {
+            $name = $this->raw->xpath('./ns1:mainHeadings/ns1:data/ns1:text');
+            return (string) $name[0];
+        } else {
+            $this->logger->warning('No name found for given VIAF URL');
+            return false;
+        }
+    }
+
+    /**
+     * Get the VIAF raw profile data
+     *
+     * @return void
+     **/
+    protected function getRaw()
+    {
+        $data = $this->client->getData();
+        if (!isset($this->raw) && $data != false) {
+            $this->raw = Helper::getXmlFileAsString($data);
+            $this->raw->registerXPathNamespace('ns1', 'http://viaf.org/viaf/terms#');
+        }
+    }
+}
