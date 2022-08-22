60c8230363406ac36666272370c3918756eb87c8
Kajetan Dvoracek <kajetan.dvoracek@slub-dresden.de>
2022-08-22 21:20:55 +0200

    Make loaded document a global variable
    This will allow the navigation plugin to access the document, e.g., to
    get the number of pages.

--- Classes/Controller/PageViewController.php
+++ Classes/Controller/PageViewController.php
@@ -503,7 +503,6 @@ class PageViewController extends AbstractController
                         'div' => "tx-dfgviewer-map-' . $i . '",
                         'progressElementId' => $this->settings['progressElementId'],
                         'counter' => $i,
-                        'document' => $this->document->getCurrentDocument()->toArray($this->uriBuilder, $config),
                         'images' => $docImage,
                         'fulltexts' => $docFulltext,
                         'score' => $docScore,
@@ -522,6 +521,8 @@ class PageViewController extends AbstractController
 
             // Viewer configuration.
             $viewerConfiguration = '$(document).ready(function() {
+                    tx_dlf_loaded_document = ' . json_encode($this->document->getCurrentDocument()->toArray($this->uriBuilder, $config)) . ';
+
                     if (dlfUtils.exists(dlfViewer)) {
                         ' . $jsViewer . '
                         viewerCount = ' . ($i - 1) . ';
@@ -540,7 +541,6 @@ class PageViewController extends AbstractController
                 'controls' => $this->controls,
                 'div' => $this->settings['elementId'],
                 'progressElementId' => $this->settings['progressElementId'],
-                'document' => $this->document->getCurrentDocument()->toArray($this->uriBuilder, $config),
                 'images' => $this->images,
                 'fulltexts' => $this->fulltexts,
                 'score' => $this->scores,
@@ -554,6 +554,8 @@ class PageViewController extends AbstractController
 
             // Viewer configuration.
             $viewerConfiguration = '$(document).ready(function() {
+                    tx_dlf_loaded_document = ' . json_encode($this->document->getCurrentDocument()->toArray($this->uriBuilder, $config)) . ';
+
                     if (dlfUtils.exists(dlfViewer)) {
                         tx_dlf_viewer = new dlfViewer(' . json_encode($viewer) . ');
                     }
