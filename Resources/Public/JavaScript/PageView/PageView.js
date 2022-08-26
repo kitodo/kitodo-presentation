2f274ba1211913045b76396040b04d40a584be63
Matthias Richter <matthias.richter@slub-dresden.de>
2022-08-26 17:02:15 +0200

    Start moving getVisiblePages to dlfController

--- Resources/Public/JavaScript/PageView/PageView.js
+++ Resources/Public/JavaScript/PageView/PageView.js
@@ -26,6 +26,7 @@
  *  images?: dlf.ImageDesc[] | [];
  *  fulltexts?: dlf.FulltextDesc[] | [];
  *  controls?: ('OverviewMap' | 'ZoomPanel')[];
+ *  initDoc: dlf.Document;
  *  measureCoords?: MeasureDesc[] | [];
  *  measureIdLinks?: MeasureDesc[] | [];
  * }} DlfViewerConfig
@@ -260,6 +261,17 @@ var dlfViewer = function (settings) {
      * @private
      */
     this.layersCache = {};
+
+    /**
+     * @private
+     */
+    this.initDoc = settings.initDoc;
+
+    /**
+     * @type {dlfController | null}
+     * @private
+     */
+    this.docController = null;
 
     this.registerEvents();
     this.init(dlfUtils.exists(settings.controls) ? settings.controls : []);
@@ -503,7 +515,7 @@ dlfViewer.prototype.addCustomControls = function() {
 
     // Adds fulltext behavior and download only if there is fulltext available and no double page
     // behavior is active
-	const currentFulltext = this.fulltextsLoaded_[`${tx_dlf_loaded.getVisiblePages()[0].pageNo}-0`];
+	const currentFulltext = this.fulltextsLoaded_[`${this.getVisiblePages()[0].pageNo}-0`];
     this.updateFulltext(currentFulltext);
 
     if (this.scoresLoaded_ !== undefined) {
@@ -818,7 +830,7 @@ dlfViewer.prototype.displayHighlightWord = function(highlightWords = null) {
         var self = this;
         var values = decodeURIComponent(this.highlightWords).split(';');
 
-		const currentFulltext = this.fulltextsLoaded_[tx_dlf_loaded.getVisiblePages()[0].pageNo];
+		const currentFulltext = this.fulltextsLoaded_[this.getVisiblePages()[0].pageNo];
         $.when.apply($, currentFulltext)
             .done(function (fulltextData, fulltextDataImageTwo) {
                 var stringFeatures = [];
@@ -856,7 +868,7 @@ dlfViewer.prototype.init = function(controlNames) {
         .done($.proxy(function(layers){
 
             // Initiate loading fulltexts
-            this.initLoadFulltexts(tx_dlf_loaded.getVisiblePages());
+            this.initLoadFulltexts(this.getVisiblePages());
             this.initLoadScores();
 
             var controls = controlNames.length > 0 || controlNames[0] === ""
@@ -943,9 +955,24 @@ dlfViewer.prototype.init = function(controlNames) {
         this.initCropping();
 };
 
+dlfViewer.prototype.getVisiblePages = function () {
+    if (this.docController === null) {
+        return this.initDoc.pages.map( (page, i) => ({
+            pageNo: this.initDoc.query.minPage + i,
+            pageObj: page
+        }))
+    } else {
+        return this.docController.getVisiblePages();
+    }
+}
+
+dlfViewer.prototype.setDocController = function (docController) {
+    this.docController = docController;
+}
+
 dlfViewer.prototype.registerEvents = function () {
     $(document.body).on('tx-dlf-stateChanged', () => {
-        this.loadPages(tx_dlf_loaded.getVisiblePages());
+        this.loadPages(this.getVisiblePages());
     });
 };
 
