69f1a4fa161163e2ce4ed1a793de47d5128b74cf
Kajetan Dvoracek <kajetan.dvoracek@slub-dresden.de>
2022-08-28 00:10:30 +0200

    Convert `TODO` to `TODO(client-side)`

--- Classes/Controller/ToolboxController.php
+++ Classes/Controller/ToolboxController.php
@@ -224,6 +224,7 @@ class ToolboxController extends AbstractController
      *
      * @return void
      */
+    // TODO(client-side)
     private function renderAnnotationTool(): void
     {
         if ($this->isDocMissingOrEmpty()) {
