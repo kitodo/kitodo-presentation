a9aa9c497924913a76a434d8090852fd5681abf1
Beatrycze Volk <beatrycze.volk@slub-dresden.de>
2023-01-27 14:44:34 +0100

    Update bash script for download

--- Resources/Public/Javascript/OpenLayers/get-ol.sh
+++ Resources/Public/Javascript/OpenLayers/get-ol.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-VERSION="6.12.0"
+VERSION="7.2.2"
 
-wget -O ol.zip "https://github.com/openlayers/openlayers/releases/download/v$VERSION/v$VERSION-dist.zip"
-unzip -p ol.zip "v$VERSION-dist/ol.js" > openlayers.js
-unzip -p ol.zip "v$VERSION-dist/ol.css" > openlayers.css
+wget -O ol.zip "https://github.com/openlayers/openlayers/releases/download/v$VERSION/v$VERSION-package.zip"
+unzip -p ol.zip "v$VERSION-package/dist/ol.js" > openlayers.js
+unzip -p ol.zip "v$VERSION-package/ol.css" > openlayers.css
 rm ol.zip
