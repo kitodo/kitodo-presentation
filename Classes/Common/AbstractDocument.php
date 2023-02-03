0898f96d1824e611f2ba9a7de94492265d6c0f91
Beatrycze Volk <beatrycze.volk@slub-dresden.de>
2023-02-03 17:38:41 +0100

    Fix toArray method in Doc class
    - remove dead variables
    - fix check for mime type

--- Classes/Common/AbstractDocument.php
+++ Classes/Common/AbstractDocument.php
@@ -1260,9 +1260,6 @@ abstract class AbstractDocument
                 'minPage' => $minPage
             ]
         ];
-        $extConf = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get(self::$extKey);
-        $fileGrpsImages = array_reverse(GeneralUtility::trimExplode(',', $extConf['fileGrpImages']));
-        $fileGrpsFulltext = GeneralUtility::trimExplode(',', $extConf['fileGrpFulltext']);
 
         $allFiles = $this->getAllFiles();
 
