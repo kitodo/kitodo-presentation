855255bcba581f80c45e34b15c1537eaf795f09f
Beatrycze Volk <beatrycze.volk@slub-dresden.de>
2023-02-03 17:38:41 +0100

    Fix toArray method in Doc class
    - remove dead variables
    - fix check for mime type

--- Classes/Common/AbstractDocument.php
+++ Classes/Common/AbstractDocument.php
@@ -1386,9 +1386,6 @@ abstract class AbstractDocument
                 'minPage' => $minPage
             ]
         ];
-        $extConf = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get(self::$extKey);
-        $fileGrpsImages = array_reverse(GeneralUtility::trimExplode(',', $extConf['fileGrpImages']));
-        $fileGrpsFulltext = GeneralUtility::trimExplode(',', $extConf['fileGrpFulltext']);
 
         $allFiles = $this->getAllFiles();
 
@@ -1412,8 +1409,8 @@ abstract class AbstractDocument
                 }
 
                 // Only deliver static images via the internal PageViewProxy.
-                // (For IIP and IIIF, the viewer needs to build and access a separate metadata URL, see `getMetdadataURL`.)
-                if (in_array($fileGrp, $proxyFileGroups) && !in_array($mimetype, self::$nonProxyMimeTypes)) {
+                // (For IIP and IIIF, the viewer needs to build and access a separate metadata URL, see `getMetadataURL`.)
+                if (in_array($fileGrp, $proxyFileGroups) && !in_array($file['mimetype'], self::$nonProxyMimeTypes)) {
                     // Configure @action URL for form.
                     $file['url'] = $uriBuilder
                         ->reset()
