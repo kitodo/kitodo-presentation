fd6ceb85e2a278d19923704f3136caa8616f4be8
Christos Sidiropoulos <csidirop@runbox.com>
2023-02-13 13:49:34 +0000

    Add 'SameSite' attribute to cookies
    Signed-off-by: Christos Sidiropoulos <csidirop@runbox.com>

--- Resources/Public/JavaScript/PageView/FulltextControl.js
+++ Resources/Public/JavaScript/PageView/FulltextControl.js
@@ -446,7 +446,7 @@ dlfViewerFullTextControl.prototype.activate = function() {
 
     // now activate the fulltext overlay and map behavior
     this.enableFulltextSelect();
-    dlfUtils.setCookie("tx-dlf-pageview-fulltext-select", 'enabled');
+    dlfUtils.setCookie("tx-dlf-pageview-fulltext-select", 'enabled', "lax");
     $(controlEl).addClass('active');
     this.isActive = true;
 
@@ -463,7 +463,7 @@ dlfViewerFullTextControl.prototype.deactivate = function() {
 
     // deactivate fulltext
     this.disableFulltextSelect();
-    dlfUtils.setCookie("tx-dlf-pageview-fulltext-select", 'disabled');
+    dlfUtils.setCookie("tx-dlf-pageview-fulltext-select", 'disabled', "lax");
     $(controlEl).removeClass('active');
     this.isActive = false;
 
