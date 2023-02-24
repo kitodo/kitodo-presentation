4d732a432663f294f31f4e42a36ccddd22d607c5
Christos Sidiropoulos <csidirop@runbox.com>
2023-02-24 13:22:42 +0000

    Add fallback option for getLLL
    Adds a fallback option in getLLL() if there is no translation for given key
    
    Signed-off-by: Christos Sidiropoulos <csidirop@runbox.com>

--- Classes/Controller/Backend/NewTenantController.php
+++ Classes/Controller/Backend/NewTenantController.php
@@ -419,8 +419,10 @@ class NewTenantController extends AbstractController
     {
         if (isset($langArray[$lang][$index][0]['target'])) {
             return $langArray[$lang][$index][0]['target'];
-        } else {
+        } else if (isset($langArray['default'][$index][0]['target'])) {
             return $langArray['default'][$index][0]['target'];
+        } else {
+            return "Missing translation for ".substr($index, 9); //cut 'metadata.' string
         }
     }
 }
