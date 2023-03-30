e0c084815757471bc0ea74a074a2909948f8918b
Beatrycze Volk <beatrycze.volk@slub-dresden.de>
2023-03-30 14:57:33 +0200

    Split parsing of metadata in printMetadata method

--- Classes/Controller/MetadataController.php
+++ Classes/Controller/MetadataController.php
@@ -137,69 +137,12 @@ class MetadataController extends AbstractController
                 'is_listed' => !$this->settings['showFull'],
             ]);
 
-            $buildUrl = [];
-            $externalUrl = [];
-
             foreach ($metadata as $i => $section) {
 
                 foreach ($section as $name => $value) {
                     // NOTE: Labels are to be escaped in Fluid template
 
-                    if ($name == 'title') {
-                        // Get title of parent document if needed.
-                        if (empty(implode('', $value)) && $this->settings['getTitle'] && $this->document->getPartof()) {
-                            $superiorTitle = Doc::getTitle($this->document->getPartof(), true);
-                            if (!empty($superiorTitle)) {
-                                $metadata[$i][$name] = ['[' . $superiorTitle . ']'];
-                            }
-                        }
-                        if (!empty($value)) {
-                            $metadata[$i][$name][0] = $metadata[$i][$name][0];
-                            // Link title to pageview.
-                            if ($this->settings['linkTitle'] && $section['_id']) {
-                                $details = $this->doc->getLogicalStructure($section['_id']);
-                                $buildUrl[$i][$name]['buildUrl'] = [
-                                    'id' => $this->document->getUid(),
-                                    'page' => (!empty($details['points']) ? intval($details['points']) : 1),
-                                    'targetPid' => (!empty($this->settings['targetPid']) ? $this->settings['targetPid'] : 0),
-                                ];
-                            }
-                        }
-                    } elseif (($name == 'author' || $name == 'holder') && !empty($value) && !empty($value[0]['url'])) {
-                        $externalUrl[$i][$name]['externalUrl'] = $value[0];
-                    } elseif (($name == 'geonames' || $name == 'wikidata' || $name == 'wikipedia') && !empty($value)) {
-                        $externalUrl[$i][$name]['externalUrl'] = [
-                            'name' => $value[0],
-                            'url' => $value[0]
-                        ];
-                    } elseif ($name == 'owner' && empty($value)) {
-                        // no owner is found by metadata records --> take the one associated to the document
-                        $library = $this->document->getOwner();
-                        if ($library) {
-                            $metadata[$i][$name][0] = $library->getLabel();
-                        }
-                    } elseif ($name == 'type' && !empty($value)) {
-                        // Translate document type.
-                        $structure = $this->structureRepository->findOneByIndexName($metadata[$i][$name][0]);
-                        if ($structure) {
-                            $metadata[$i][$name][0] = $structure->getLabel();
-                        }
-                    } elseif ($name == 'collection' && !empty($value)) {
-                        // Check if collections isn't hidden.
-                        $j = 0;
-                        foreach ($value as $entry) {
-                            $collection = $this->collectionRepository->findOneByIndexName($entry);
-                            if ($collection) {
-                                $metadata[$i][$name][$j] = $collection->getLabel() ? : '';
-                                $j++;
-                            }
-                        }
-                    } elseif ($name == 'language' && !empty($value)) {
-                        // Translate ISO 639 language code.
-                        foreach ($metadata[$i][$name] as &$langValue) {
-                            $langValue = Helper::getLanguageName($langValue);
-                        }
-                    }
+                    $this->parseMetadata($i, $name, $value, $metadata);
 
                     if (is_array($metadata[$i][$name])) {
                         $metadata[$i][$name] = array_values(array_filter($metadata[$i][$name], function($metadataValue)
@@ -208,11 +151,10 @@ class MetadataController extends AbstractController
                         }));
                     }
                 }
-                
             }
 
-            $this->view->assign('buildUrl', $buildUrl);
-            $this->view->assign('externalUrl', $externalUrl);
+            $this->view->assign('buildUrl', $this->buildUrlFromMetadata($metadata));
+            $this->view->assign('externalUrl', $this->buildExternalUrlFromMetadata($metadata));
             $this->view->assign('documentMetadataSections', $metadata);
             $this->view->assign('configMetadata', $metadataResult);
             $this->view->assign('separator', $this->settings['separator']);
@@ -312,6 +254,113 @@ class MetadataController extends AbstractController
         }
 
         return $metaCObjData;
+    }
+
+    /**
+     * Builds URLs array for given metadata array.
+     *
+     * @access private
+     *
+     * @param array $metadata The metadata array
+     *
+     * @return array URLs
+     */
+    private function buildUrlFromMetadata(array $metadata)
+    {
+        $buildUrl = [];
+
+        foreach ($metadata as $i => $section) {
+            if ($this->settings['linkTitle'] && $section['_id'] && isset($section['title']) && !empty($section['title'])) {
+                $details = $this->doc->getLogicalStructure($section['_id']);
+                $buildUrl[$i]['title'] = [
+                    'id' => $this->document->getUid(),
+                    'page' => (!empty($details['points']) ? intval($details['points']) : 1),
+                    'targetPid' => (!empty($this->settings['targetPid']) ? $this->settings['targetPid'] : 0),
+                ];
+            }
+        }
+
+        return $buildUrl;
+    }
+
+    /**
+     * Builds external URLs array for given metadata array.
+     *
+     * @access private
+     *
+     * @param array $metadata The metadata array
+     *
+     * @return array external URLs
+     */
+    private function buildExternalUrlFromMetadata(array $metadata)
+    {
+        $externalUrl = [];
+
+        foreach ($metadata as $i => $section) {
+            foreach ($section as $name => $value) {
+                if (($name == 'author' || $name == 'holder') && !empty($value) && !empty($value[0]['url'])) {
+                    $externalUrl[$i][$name]['externalUrl'] = $value[0];
+                } elseif (($name == 'geonames' || $name == 'wikidata' || $name == 'wikipedia') && !empty($value)) {
+                    $externalUrl[$i][$name]['externalUrl'] = [
+                        'name' => $value[0],
+                        'url' => $value[0]
+                    ];
+                }
+            }
+        }
+
+        return $externalUrl;
+    }
+
+    /**
+     * Parses metadata.
+     *
+     * @access private
+     *
+     * @param int $i The index of metadata array
+     * @param string $name The name of section in metadata array
+     * @param mixed $value The value of section in metadata array
+     * @param array $metadata The metadata array passed as reference
+     *
+     * @return void
+     */
+    private function parseMetadata(int $i, string $name, $value, array &$metadata) : void {
+        if ($name == 'title') {
+            // Get title of parent document if needed.
+            if (empty(implode('', $value)) && $this->settings['getTitle'] && $this->document->getPartof()) {
+                $superiorTitle = Doc::getTitle($this->document->getPartof(), true);
+                if (!empty($superiorTitle)) {
+                    $metadata[$i][$name] = ['[' . $superiorTitle . ']'];
+                }
+            }
+        } elseif ($name == 'owner' && empty($value)) {
+            // no owner is found by metadata records --> take the one associated to the document
+            $library = $this->document->getOwner();
+            if ($library) {
+                $metadata[$i][$name][0] = $library->getLabel();
+            }
+        } elseif ($name == 'type' && !empty($value)) {
+            // Translate document type.
+            $structure = $this->structureRepository->findOneByIndexName($metadata[$i][$name][0]);
+            if ($structure) {
+                $metadata[$i][$name][0] = $structure->getLabel();
+            }
+        } elseif ($name == 'collection' && !empty($value)) {
+            // Check if collections isn't hidden.
+            $j = 0;
+            foreach ($value as $entry) {
+                $collection = $this->collectionRepository->findOneByIndexName($entry);
+                if ($collection) {
+                    $metadata[$i][$name][$j] = $collection->getLabel() ? : '';
+                    $j++;
+                }
+            }
+        } elseif ($name == 'language' && !empty($value)) {
+            // Translate ISO 639 language code.
+            foreach ($metadata[$i][$name] as &$langValue) {
+                $langValue = Helper::getLanguageName($langValue);
+            }
+        }
     }
 
     /**
