8a30d812870eedfeed33d981b4c22fa27f7a42d7
Christopher Timm <timm@effective-webwork.de>
2023-06-30 17:03:10 +0200

    Add gridstack lib

---
+++ Resources/Public/Javascript/Gridstack/gridstack.js
@@ -0,0 +1,47 @@
+$( document ).ready(function() {
+    var options = { // put in gridstack options here
+        disableOneColumnMode: true, // for jfiddle small window size
+        float: false
+    };
+    var grid = GridStack.init(options);
+
+    // DEV #########################
+    // var count = 0;
+    // var items = [
+    //     {x: 0, y: 0, w: 2, h: 2},
+    //     {x: 2, y: 0, w: 2},
+    //     {x: 3, y: 1, h: 2},
+    //     {x: 0, y: 2, w: 2},
+    // ];
+    //
+    // addNewWidget = function () {
+    //     var node = items[count] || {
+    //         x: Math.round(12 * Math.random()),
+    //         y: Math.round(5 * Math.random()),
+    //         w: Math.round(1 + 3 * Math.random()),
+    //         h: Math.round(1 + 3 * Math.random())
+    //     };
+    //     node.content = String(count++);
+    //     grid.addWidget(node);
+    //     return false;
+    // };
+
+    // DEV ########################
+
+    $("#enableEdit").on('click', function (evt) {
+        grid.enableMove(true);
+        grid.enableResize(true);
+    });
+
+    $("#disableEdit").on('click', function (evt) {
+        grid.enableMove(false);
+        grid.enableResize(false);
+    });
+
+    // resize each map
+    grid.on('change', function(evt, items) {
+        $('.tx-dlf-map').each(function (index) {
+            tx_dlf_viewer[index].map.updateSize()
+        });
+    });
+});
