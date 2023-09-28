5e8c1665b6c23be6f1533cb3bc8ca0513a0f2617
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2023-09-28 22:49:33 +0200

    Update BaseCommand.php

--- Classes/Command/BaseCommand.php
+++ Classes/Command/BaseCommand.php
@@ -107,7 +107,7 @@ class BaseCommand extends Command
     /**
      * Initialize the extbase repository based on the given storagePid.
      *
-     * TYPO3 10+: Find a better solution e.g. based on Symfonie Dependency Injection.
+     * TYPO3 10+: Find a better solution e.g. based on Symfony Dependency Injection.
      *
      * @param int $storagePid The storage pid
      *
