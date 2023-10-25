27b2db10d722c73978da4f3ef9e7781d1da99786
Oliver Stöhr <stoehr@effective-webwork.de>
2023-10-25 17:34:46 +0200

    Fix AudioVideoMDTest

--- Tests/Unit/Format/AudioVideoMDTest.php
+++ Tests/Unit/Format/AudioVideoMDTest.php
@@ -5,7 +5,7 @@ namespace Kitodo\Dlf\Tests\Unit\Format;
 use Kitodo\Dlf\Format\AudioVideoMD;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 
-class AudioVideaoMDTest extends UnitTestCase
+class AudioVideoMDTest extends UnitTestCase
 {
     protected $metadata = [];
 
@@ -24,86 +24,22 @@ class AudioVideaoMDTest extends UnitTestCase
      * @test
      * @group extractMetadata
      */
-    public function canExtractVideoDuration(): void
+    public function canExtractDuration(): void
     {
         $xml = simplexml_load_file( __DIR__ . '/../../Fixtures/Format/audioVideo.xml');
         $audioVideoMD = new AudioVideoMD();
 
-        $videoXml = $xml->xpath('//videomd:VIDEOMD')[0];
+        $videoXml = $xml->xpath('//mets:xmlData')[0];
 
         $audioVideoMD->extractMetadata($videoXml,$this->metadata);
 
         $this->assertEquals(
-            ["00:01:30.07"],
-            $this->metadata['duration']
-        );
-
-        $this->assertEquals(
-            ["00:01:30.07"],
-            $this->metadata['video_duration']
-        );
-
-        $this->assertEquals(
-            [],
-            $this->metadata['audio_duration']
-        );
-    }
-
-    /**
-     * @test
-     * @group extractMetadata
-     */
-    public function canExtractAudioDuration(): void
-    {
-        $xml = simplexml_load_file( __DIR__ . '/../../Fixtures/Format/audioVideo.xml');
-        $audioVideoMD = new AudioVideoMD();
-
-        $audioXml = $xml->xpath('//audiomd:AUDIOMD')[0];
-
-        $audioVideoMD->extractMetadata($audioXml,$this->metadata);
-
-        $this->assertEquals(
-            ["01:10:35.08"],
-            $this->metadata['duration']
-        );
-
-        $this->assertEquals(
-            [],
-            $this->metadata['video_duration']
-        );
-
-        $this->assertEquals(
-            ["01:10:35.08"],
-            $this->metadata['audio_duration']
-        );
-    }
-
-    /**
-     * @test
-     * @group extractMetadata
-     */
-    public function noDuration(): void
-    {
-        $xml = simplexml_load_file( __DIR__ . '/../../Fixtures/Format/audioVideo.xml');
-        $audioVideoMD = new AudioVideoMD();
-
-        $videoXml = $xml->xpath('//audiomd:AUDIOMD')[1];
-
-        $audioVideoMD->extractMetadata($videoXml,$this->metadata);
-
-        $this->assertEquals(
-            [],
-            $this->metadata['duration']
-        );
-
-        $this->assertEquals(
-            [],
-            $this->metadata['audio_duration']
-        );
-
-        $this->assertEquals(
-            [],
-            $this->metadata['video_duration']
+            [
+                'duration' => ["00:01:30.07"],
+                'video_duration' => ["00:01:30.07"],
+                'audio_duration' => ["01:10:35.08"]
+            ],
+            $this->metadata
         );
     }
 }
