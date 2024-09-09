c4345ec2317e85d8987fe1273249ce4437bac9c3
Bernd Fallert <bernd.fallert@bib.uni-mannheim.de>
2024-09-09 15:59:31 +0200

    [BUGFIX ] Translation of structure type doesn't work (#1316)
    Co-authored-by: Bernd Fallert <bernd.fallert@uni-mannheim.de>

--- Classes/Controller/MetadataController.php
+++ Classes/Controller/MetadataController.php
@@ -437,9 +437,9 @@ class MetadataController extends AbstractController
      */
     private function parseType(int $i, array &$metadata) : void
     {
-        $structure = $this->structureRepository->findOneByIndexName($metadata[$i]['type'][0]);
+        $structure = $this->structureRepository->findOneByIndexName($metadata[$i]['type']);
         if ($structure) {
-            $metadata[$i]['type'][0] = $structure->getLabel();
+            $metadata[$i]['type'] = $structure->getLabel();
         }
     }
 
