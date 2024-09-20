0377a5ee6683c595cba4bacc90d932ed5169a537
Beatrycze Volk <beatrycze.volk@slub-dresden.de>
2024-09-20 10:55:34 +0200

    [BUGFIX] Call `sanitizeSettings` function in `loadDocument` of `AbstractController` (#1334)
    Co-authored-by: Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>

--- Classes/Controller/AbstractController.php
+++ Classes/Controller/AbstractController.php
@@ -126,6 +126,9 @@ abstract class AbstractController extends ActionController implements LoggerAwar
      */
     protected function loadDocument(int $documentId = 0): void
     {
+        // Sanitize FlexForm settings to avoid later casting.
+        $this->sanitizeSettings();
+
         // Get document ID from request data if not passed as parameter.
         if ($documentId === 0 && !empty($this->requestData['id'])) {
             $documentId = $this->requestData['id'];
@@ -313,7 +316,7 @@ abstract class AbstractController extends ActionController implements LoggerAwar
      */
     protected function setDefaultIntSetting(string $setting, int $value): void
     {
-        if (empty($this->settings[$setting])) {
+        if (!array_key_exists($setting, $this->settings) || empty($this->settings[$setting])) {
             $this->settings[$setting] = $value;
             $this->logger->warning('Setting "' . $setting . '" not set, using default value "' . $value . '". Probably FlexForm for controller "' . get_class($this) . '" is not read.');
         } else {
