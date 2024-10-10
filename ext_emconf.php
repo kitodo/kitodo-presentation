c9bb7031dca8fafb12dcf23793b2e0ac64eb26ae
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2024-10-10 12:02:51 +0200

    [RELEASE] Release v5.0.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Kitodo.Presentation',
     'description' => 'Base plugins, modules, services and API of the Digital Library Framework. It is part of the community-based Kitodo Digitization Suite.',
-    'version' => '5.0.5',
+    'version' => '5.0.6',
     'category' => 'misc',
     'constraints' => [
         'depends' => [
