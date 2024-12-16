0a2ed76cc72de74f3d0a5ada9aa950b61fdaf3c7
Beatrycze Volk <beatrycze.volk@slub-dresden.de>
2024-12-16 21:46:52 +0100

    [BUGFIX] Fix collection search (#1396)
    Co-authored-by: Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>

--- Classes/Controller/SearchController.php
+++ Classes/Controller/SearchController.php
@@ -395,7 +395,9 @@ class SearchController extends AbstractController
         $collections = null;
         if (array_key_exists('collection', $this->searchParams)) {
             foreach (explode(',', $this->searchParams['collection']) as $collectionEntry) {
-                $collections[] = $this->collectionRepository->findByUid((int) $collectionEntry);
+                if (!empty($collectionEntry)) {
+                    $collections[] = $this->collectionRepository->findByUid((int) $collectionEntry);
+                }
             }
         }
         if ($collections) {
