c115c1971312dcb6dbc5adc801e04242a6e0ddbd
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2025-03-05 08:24:32 +0100

    Update Classes/Command/DbDocs/RstSection.php

--- Classes/Command/DbDocs/RstSection.php
+++ Classes/Command/DbDocs/RstSection.php
@@ -10,7 +10,7 @@
  * LICENSE.txt file that was distributed with this source code.
  */
 
- namespace Kitodo\Dlf\Command\DbDocs;
+namespace Kitodo\Dlf\Command\DbDocs;
 
 /**
  * Simple utility to write .rst (reStructuredText).
