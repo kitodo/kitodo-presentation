kitodo/kitodo-presentation

commit 27812ee974264d5c4d1e10ed693668383a4c3014

[FEATURE] Implement `UseGroupsConfiguration` class (#1422)
Co-authored-by: Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>

Classes/Common/AbstractDocument.php
@@ -12,6 +12,7 @@
 
 namespace Kitodo\Dlf\Common;
 
+use Kitodo\Dlf\Configuration\UseGroupsConfiguration;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Log\Logger;
@@ -275,12 +276,12 @@ abstract class AbstractDocument
     protected string $toplevelId = '';
 
     /**
-     * Holds the configured useGrps as array.
+     * Holds the configured useGroups as array.
      *
-     * @var array
      * @access protected
+     * @var \Kitodo\Dlf\Configuration\UseGroupsConfiguration
      */
-    protected array $useGroups = [];
+    protected UseGroupsConfiguration $useGroupsConfiguration;
 
     /**
      * @access protected
@@ -621,20 +622,6 @@ abstract class AbstractDocument
     }
 
     /**
-     * Get the configuration for given use group.
-     *
-     * @access protected
-     *
-     * @param string $use
-     *
-     * @return array|string
-     */
-    protected function getUseGroup(string $use)
-    {
-        return array_key_exists($use, $this->getUseGroups()) ? $this->useGroups[$use] : [];
-    }
-
-    /**
      * This determines a title for the given document
      *
      * @access public
@@ -758,37 +745,6 @@ abstract class AbstractDocument
     public function getStructureDepth(string $logId)
     {
         return $this->getTreeDepth($this->magicGetTableOfContents(), 1, $logId);
-    }
-
-    /**
-     * Get the configuration for use groups.
-     *
-     * @access protected
-     *
-     * @return array
-     */
-    protected function getUseGroups(): array
-    {
-        if (empty($this->useGroups)) {
-            // Get configured USE attributes.
-            $extConf = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get(self::$extKey, 'files');
-
-            $configKeys = [
-                'fileGrpImages',
-                'fileGrpThumbs',
-                'fileGrpDownload',
-                'fileGrpFulltext',
-                'fileGrpAudio',
-                'fileGrpScore'
-            ];
-
-            foreach ($configKeys as $key) {
-                if (!empty($extConf[$key])) {
-                    $this->useGroups[$key] = GeneralUtility::trimExplode(',', $extConf[$key]);
-                }
-            }
-        }
-        return $this->useGroups;
     }
 
     /**
@@ -1141,6 +1097,7 @@ abstract class AbstractDocument
     protected function __construct(int $pid, string $location, $preloadedDocument, array $settings = [])
     {
         $this->pid = $pid;
+        $this->useGroupsConfiguration = UseGroupsConfiguration::getInstance();
         $this->setPreloadedDocument($preloadedDocument);
         $this->init($location, $settings);
         $this->establishRecordId($pid);

Classes/Common/IiifManifest.php
@@ -223,8 +223,8 @@ final class IiifManifest extends AbstractDocument
             $this->setFileUseDownload($iiifId, $this->iiif);
             $this->setFileUseFulltext($iiifId, $this->iiif);
 
-            $fileUseThumbs = $this->getUseGroup('fileGrpThumbs');
-            $fileUses = $this->getUseGroup('fileGrpImages');
+            $fileUseThumbs = $this->useGroupsConfiguration->getThumbnail();
+            $fileUses = $this->useGroupsConfiguration->getImage();
 
             if (!empty($this->iiif->getDefaultCanvases())) {
                 // canvases have not order property, but the context defines canveses as @list with a specific order, so we can provide an alternative
@@ -697,16 +697,15 @@ final class IiifManifest extends AbstractDocument
             // Load physical structure ...
             $this->magicGetPhysicalStructure();
             // ... and extension configuration.
-            $extConf = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get(self::$extKey);
-            $fileGrpsFulltext = $this->getUseGroup('fileGrpFulltext');
+            $useGroups = $this->useGroupsConfiguration->getFulltext();
 
             $physicalStructureNode = $this->physicalStructureInfo[$id];
             if (!empty($physicalStructureNode)) {
-                while ($fileGrpFulltext = array_shift($fileGrpsFulltext)) {
-                    if (!empty($physicalStructureNode['files'][$fileGrpFulltext])) {
+                while ($useGroup = array_shift($useGroups)) {
+                    if (!empty($physicalStructureNode['files'][$useGroup])) {
                         $rawText = GeneralUtility::makeInstance(FullTextReader::class, $this->formats)->getFromXml(
                             $id,
-                            [$fileGrpFulltext => $this->getFileLocation($physicalStructureNode['files'][$fileGrpFulltext])],
+                            [$useGroup => $this->getFileLocation($physicalStructureNode['files'][$useGroup])],
                             $physicalStructureNode
                         );
                         break;
@@ -908,12 +907,12 @@ final class IiifManifest extends AbstractDocument
      */
     private function setFileUseDownload(string $iiifId, $iiif): void
     {
-        $fileUseDownload = $this->getUseGroup('fileGrpDownload');
+        $useGroups = $this->useGroupsConfiguration->getDownload();
 
-        if (!empty($fileUseDownload)) {
+        if (!empty($useGroups)) {
             $docPdfRendering = $iiif->getRenderingUrlsForFormat('application/pdf');
             if (!empty($docPdfRendering)) {
-                $this->physicalStructureInfo[$iiifId]['files'][$fileUseDownload[0]] = $docPdfRendering[0];
+                $this->physicalStructureInfo[$iiifId]['files'][$useGroups[0]] = $docPdfRendering[0];
             }
         }
     }
@@ -930,16 +929,16 @@ final class IiifManifest extends AbstractDocument
      */
     private function setFileUseFulltext(string $iiifId, $iiif): void
     {
-        $fileUseFulltext = $this->getUseGroup('fileGrpFulltext');
+        $useGroups = $this->useGroupsConfiguration->getFulltext();
 
-        if (!empty($fileUseFulltext)) {
+        if (!empty($useGroups)) {
             $alto = $iiif->getSeeAlsoUrlsForFormat('application/alto+xml');
             if (empty($alto)) {
                 $alto = $iiif->getSeeAlsoUrlsForProfile('http://www.loc.gov/standards/alto/', true);
             }
             if (!empty($alto)) {
                 $this->mimeTypes[$alto[0]] = 'application/alto+xml';
-                $this->physicalStructureInfo[$iiifId]['files'][$fileUseFulltext[0]] = $alto[0];
+                $this->physicalStructureInfo[$iiifId]['files'][$useGroups[0]] = $alto[0];
                 $this->hasFulltext = true;
                 $this->hasFulltextSet = true;
             }

Classes/Common/MetsDocument.php
@@ -23,7 +23,6 @@ use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\Restriction\HiddenRestriction;
 use TYPO3\CMS\Core\Log\LogManager;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use Ubl\Iiif\Tools\IiifHelper;
 use Ubl\Iiif\Services\AbstractImageService;
 
 /**
@@ -480,22 +479,17 @@ final class MetsDocument extends AbstractDocument
      */
     private function getThumbnail(string $id = '')
     {
-        $fileGrpsThumb = $this->getUseGroup('fileGrpThumbs');
-
+        $useGroups = $this->useGroupsConfiguration->getThumbnail();
         $thumbnail = null;
 
         if (!empty($this->physicalStructure)) {
             // There is a physical structure (no anchor or year mets).
-            while ($fileGrpThumb = array_shift($fileGrpsThumb)) {
+            while ($useGroup = array_shift($useGroups) && $thumbnail == null) {
                 if (empty($id)) {
-                    $thumbnail = $this->physicalStructureInfo[$this->physicalStructure[1]]['files'][$fileGrpThumb] ?? null;
+                    $thumbnail = $this->physicalStructureInfo[$this->physicalStructure[1]]['files'][$useGroup] ?? null;
                 } else {
                     $parentId = $this->smLinks['l2p'][$id][0] ?? null;
-                    $thumbnail = $this->physicalStructureInfo[$parentId]['files'][$fileGrpThumb] ?? null;
-                }
-
-                if (!empty($thumbnail)) {
-                    break;
+                    $thumbnail = $this->physicalStructureInfo[$parentId]['files'][$useGroup] ?? null;
                 }
             }
         }
@@ -1125,15 +1119,13 @@ final class MetsDocument extends AbstractDocument
         $this->ensureHasFulltextIsSet();
 
         if ($this->hasFulltext) {
-            $extConf = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get(self::$extKey, 'files');
-            $fileGrpsFulltext = GeneralUtility::trimExplode(',', $extConf['fileGrpFulltext']);
-
+            $useGroups = $this->useGroupsConfiguration->getFulltext();
             $physicalStructureNode = $this->physicalStructureInfo[$id];
-
             $fileLocations = [];
+
             if (!empty($physicalStructureNode)) {
-                while ($fileGrpFulltext = array_shift($fileGrpsFulltext)) {
-                    $fileLocations[$fileGrpFulltext] = $this->getFileLocation($physicalStructureNode['files'][$fileGrpFulltext]);
+                while ($useGroup = array_shift($useGroups)) {
+                    $fileLocations[$useGroup] = $this->getFileLocation($physicalStructureNode['files'][$useGroup]);
                 }
             }
 
@@ -1206,8 +1198,8 @@ final class MetsDocument extends AbstractDocument
     {
         // Are there any fulltext files available?
         if (
-            !empty($this->getUseGroup('fileGrpFulltext'))
-            && array_intersect($this->getUseGroup('fileGrpFulltext'), $this->fileGrps) !== []
+            !empty($this->useGroupsConfiguration->getFulltext())
+            && array_intersect($this->useGroupsConfiguration->getFulltext(), $this->fileGrps) !== []
         ) {
             $this->hasFulltext = true;
         }
@@ -1351,7 +1343,7 @@ final class MetsDocument extends AbstractDocument
     protected function magicGetFileGrps(): array
     {
         if (!$this->fileGrpsLoaded) {
-            foreach (array_values($this->getUseGroups()) as $useGroups) {
+            foreach (array_values($this->useGroupsConfiguration->get()) as $useGroups) {
                 foreach ($useGroups as $useGroup) {
                     // Perform XPath query for each configured USE attribute
                     $fileGrps = $this->mets->xpath("./mets:fileSec/mets:fileGrp[@USE='$useGroup']");
@@ -1564,7 +1556,7 @@ final class MetsDocument extends AbstractDocument
                 return $this->thumbnail;
             }
 
-            if (empty($this->getUseGroup('fileGrpThumbs'))) {
+            if (empty($this->useGroupsConfiguration->getThumbnail())) {
                 $this->logger->warning('No fileGrp for thumbnails specified');
                 $this->thumbnailLoaded = true;
                 return $this->thumbnail;
@@ -1603,17 +1595,17 @@ final class MetsDocument extends AbstractDocument
                 // Load smLinks.
                 $this->magicGetSmLinks();
                 // Get thumbnail location.
-                $fileGrpsThumb = $this->getUseGroup('fileGrpThumbs');
-                while ($fileGrpThumb = array_shift($fileGrpsThumb)) {
+                $useGroups = $this->useGroupsConfiguration->getThumbnail();
+                while ($useGroup = array_shift($useGroups)) {
                     if (
                         $this->magicGetPhysicalStructure()
                         && !empty($this->smLinks['l2p'][$strctId])
-                        && !empty($this->physicalStructureInfo[$this->smLinks['l2p'][$strctId][0]]['files'][$fileGrpThumb])
+                        && !empty($this->physicalStructureInfo[$this->smLinks['l2p'][$strctId][0]]['files'][$useGroup])
                     ) {
-                        $this->thumbnail = $this->getFileLocation($this->physicalStructureInfo[$this->smLinks['l2p'][$strctId][0]]['files'][$fileGrpThumb]);
+                        $this->thumbnail = $this->getFileLocation($this->physicalStructureInfo[$this->smLinks['l2p'][$strctId][0]]['files'][$useGroup]);
                         break;
-                    } elseif (!empty($this->physicalStructureInfo[$this->physicalStructure[1]]['files'][$fileGrpThumb])) {
-                        $this->thumbnail = $this->getFileLocation($this->physicalStructureInfo[$this->physicalStructure[1]]['files'][$fileGrpThumb]);
+                    } elseif (!empty($this->physicalStructureInfo[$this->physicalStructure[1]]['files'][$useGroup])) {
+                        $this->thumbnail = $this->getFileLocation($this->physicalStructureInfo[$this->physicalStructure[1]]['files'][$useGroup]);
                         break;
                     }
                 }

Classes/Common/FullTextReader.php
@@ -58,13 +58,13 @@ class FullTextReader
     {
         $fullText = '';
 
-        $fileGrpsFulltext = $this->getFullTextFileGroups();
+        $useGroupsFulltext = $this->getFullTextUseGroups();
         $textFormat = "";
         if (!empty($physicalStructureNode)) {
-            while ($fileGrpFulltext = array_shift($fileGrpsFulltext)) {
-                if (!empty($physicalStructureNode['files'][$fileGrpFulltext])) {
+            while ($useGroupFulltext = array_shift($useGroupsFulltext)) {
+                if (!empty($physicalStructureNode['files'][$useGroupFulltext])) {
                     // Get full text file.
-                    $fileContent = GeneralUtility::getUrl($fileLocations[$fileGrpFulltext]);
+                    $fileContent = GeneralUtility::getUrl($fileLocations[$useGroupFulltext]);
                     if ($fileContent !== false) {
                         $textFormat = $this->getTextFormat($fileContent);
                     } else {
@@ -128,10 +128,10 @@ class FullTextReader
      *
      * @return array
      */
-    private function getFullTextFileGroups(): array
+    private function getFullTextUseGroups(): array
     {
         $extConf = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('dlf', 'files');
-        return GeneralUtility::trimExplode(',', $extConf['fileGrpFulltext']);
+        return GeneralUtility::trimExplode(',', $extConf['useGroupsFulltext']);
     }
 
     /**

Classes/Common/Indexer.php
@@ -439,10 +439,10 @@ class Indexer
             $updateQuery = self::$solr->service->createUpdate();
             $solrDoc = self::getSolrDocument($updateQuery, $document, $physicalUnit, $fullText);
             $solrDoc->setField('page', $page);
-            $fileGrpsThumb = GeneralUtility::trimExplode(',', $extConf['fileGrpThumbs']);
-            while ($fileGrpThumb = array_shift($fileGrpsThumb)) {
-                if (!empty($physicalUnit['files'][$fileGrpThumb])) {
-                    $solrDoc->setField('thumbnail', $doc->getFileLocation($physicalUnit['files'][$fileGrpThumb]));
+            $useGroupsThumbnail = GeneralUtility::trimExplode(',', $extConf['Thumbnail']);
+            while ($useGroupThumbnail = array_shift($useGroupsThumbnail)) {
+                if (!empty($physicalUnit['files'][$useGroupThumbnail])) {
+                    $solrDoc->setField('thumbnail', $doc->getFileLocation($physicalUnit['files'][$useGroupThumbnail]));
                     break;
                 }
             }

Classes/Configuration/UseGroupsConfiguration.php
@@ -1,0 +1,184 @@
+<?php
+
+/**
+ * (c) Kitodo. Key to digital objects e.V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo and TYPO3 projects.
+ *
+ * @license GNU General Public License version 3 or later.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+namespace Kitodo\Dlf\Configuration;
+
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Class for accessing and processing the configuration of USE groups.
+ * 
+ * @package TYPO3
+ * @subpackage dlf
+ *
+ * @access public
+ */
+class UseGroupsConfiguration
+{
+
+    /**
+     * @access private
+     * @var ?UseGroupsConfiguration The instance of singleton.
+     */
+    private static $instance = null;
+
+    /**
+     * @access private
+     * @var array The array of configured USE groups.
+     */
+    private array $useGroups = [];
+
+    /**
+     * Constructor for singleton.
+     *
+     * @access private
+     *
+     * @return void
+     */
+    private function __construct()
+    {
+        // Get configured USE attributes.
+        $extConf = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('dlf', 'files');
+
+        $configKeys = [
+            'useGroupsImage',
+            'useGroupsThumbnail',
+            'useGroupsDownload',
+            'useGroupsFulltext',
+            'useGroupsAudio',
+            'useGroupsScore'
+        ];
+
+        foreach ($configKeys as $key) {
+            if (!empty($extConf[$key])) {
+                $this->useGroups[$key] = GeneralUtility::trimExplode(',', $extConf[$key]);
+            }
+        }
+    }
+
+    /**
+     * Get the instance of singleton.
+     *
+     * @access public
+     *
+     * @return UseGroupsConfiguration
+     */
+    public static function getInstance(): UseGroupsConfiguration
+    {
+        if (self::$instance == null) {
+            self::$instance = new UseGroupsConfiguration();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Get the configuration for 'Audio' use groups type.
+     *
+     * @access public
+     *
+     * @return array
+     */
+    public function getAudio(): array
+    {
+        return $this->getByType('Audio');
+    }
+
+    /**
+     * Get the configuration for 'Download' use groups type.
+     *
+     * @access public
+     *
+     * @return array
+     */
+    public function getDownload(): array
+    {
+        return $this->getByType('Download');
+    }
+
+    /**
+     * Get the configuration for 'Fulltext' use groups type.
+     *
+     * @access public
+     *
+     * @return array
+     */
+    public function getFulltext(): array
+    {
+        return $this->getByType('Fulltext');
+    }
+
+    /**
+     * Get the configuration for 'Image' use groups type.
+     *
+     * @access public
+     *
+     * @return array
+     */
+    public function getImage(): array
+    {
+        return $this->getByType('Image');
+    }
+
+    /**
+     * Get the configuration for 'Score' use groups type.
+     *
+     * @access public
+     *
+     * @return array
+     */
+    public function getScore(): array
+    {
+        return $this->getByType('Score');
+    }
+
+    /**
+     * Get the configuration for 'Thumbnail' use groups type.
+     *
+     * @access public
+     *
+     * @return array
+     */
+    public function getThumbnail(): array
+    {
+        return $this->getByType('Thumbnail');
+    }
+
+    /**
+     * Get the configuration for use groups.
+     *
+     * @access public
+     *
+     * @return array
+     */
+    public function get(): array
+    {
+        return $this->useGroups;
+    }
+
+    /**
+     * Get the configuration for given use groups type.
+     *
+     * @access private
+     *
+     * @param string $useType possible values: 'Audio', 'Download', 'Fulltext', 'Image', 'Score', 'Thumbnail'
+     *
+     * @return array
+     */
+    //TODO: replace $useType with enum after dropping PHP 7.x support
+    private function getByType(string $useType): array
+    {
+        $useType = 'useGroups' . ucfirst($useType);
+        return array_key_exists($useType, $this->useGroups) ? $this->useGroups[$useType] : [];
+    }
+}

Classes/Controller/AbstractController.php
@@ -13,6 +13,7 @@ namespace Kitodo\Dlf\Controller;
 
 use Kitodo\Dlf\Common\AbstractDocument;
 use Kitodo\Dlf\Common\Helper;
+use Kitodo\Dlf\Configuration\UseGroupsConfiguration;
 use Kitodo\Dlf\Domain\Model\Document;
 use Kitodo\Dlf\Domain\Repository\DocumentRepository;
 use Psr\Http\Message\ResponseInterface;
@@ -96,6 +97,14 @@ abstract class AbstractController extends ActionController implements LoggerAwar
      * @var int
      */
     protected int $pageUid;
+
+    /**
+     * Holds the configured useGroups as array.
+     *
+     * @access protected
+     * @var \Kitodo\Dlf\Configuration\UseGroupsConfiguration
+     */
+    protected UseGroupsConfiguration $useGroupsConfiguration;
 
     /**
      * Initialize the plugin controller
@@ -201,6 +210,18 @@ abstract class AbstractController extends ActionController implements LoggerAwar
                 ]
             )
             ->build();
+    }
+
+    /**
+     * Checks if doc of its fulltext is missing or is empty (no pages)
+     *
+     * @access protected
+     *
+     * @return bool
+     */
+    protected function isDocOrFulltextMissingOrEmpty(): bool
+    {
+        return $this->isDocMissingOrEmpty() || empty($this->useGroupsConfiguration->getFulltext());
     }
 
     /**

Classes/Controller/AudioPlayerController.php
@@ -86,11 +86,11 @@ class AudioplayerController extends AbstractController
         $this->setDefaultPage();
 
         // Check if there are any audio files available.
-        $fileGrpsAudio = GeneralUtility::trimExplode(',', $this->extConf['files']['fileGrpAudio']);
-        while ($fileGrpAudio = array_shift($fileGrpsAudio)) {
+        $useGroups = $this->useGroupsConfiguration->getAudio();
+        while ($useGroup = array_shift($useGroups)) {
             $physicalStructureInfo = $this->document->getCurrentDocument()->physicalStructureInfo[$this->document->getCurrentDocument()->physicalStructure[$this->requestData['page'] ?? 0]];
-            if (array_key_exists($fileGrpAudio, $physicalStructureInfo['files'])) {
-                $fileId = $physicalStructureInfo['files'][$fileGrpAudio];
+            if (array_key_exists($useGroup, $physicalStructureInfo['files'])) {
+                $fileId = $physicalStructureInfo['files'][$useGroup];
                 if (!empty($fileId)) {
                     // Get audio data.
                     $file = $this->document->getCurrentDocument()->getFileInfo($fileId);

commit 540eacab86228b8971d2bafda864dc2044f5df98

[BUGFIX] Fix caching for classes IiifManifest and MetsDocument
Fixes: 27812ee97426 ("Implement `UseGroupsConfiguration` class")
Signed-off-by: Stefan Weil <sw@weilnetz.de>

Classes/Common/AbstractDocument.php
@@ -1096,6 +1096,9 @@ abstract class AbstractDocument
      */
     protected function __construct(int $pid, string $location, $preloadedDocument, array $settings = [])
     {
+        // Note: Any change here might require an update in function __sleep
+        // of class MetsDocument and class IiifManifest, too.
+
         $this->pid = $pid;
         $this->useGroupsConfiguration = UseGroupsConfiguration::getInstance();
         $this->setPreloadedDocument($preloadedDocument);

Classes/Common/IiifManifest.php
@@ -976,6 +976,6 @@ final class IiifManifest extends AbstractDocument
         // TODO implement serialization in IIIF library
         $jsonArray = $this->iiif->getOriginalJsonArray();
         $this->asJson = json_encode($jsonArray);
-        return ['pid', 'recordId', 'parentId', 'asJson'];
+        return ['pid', 'recordId', 'parentId', 'useGroupsConfiguration', 'asJson'];
     }
 }

Classes/Common/MetsDocument.php
@@ -1672,7 +1672,7 @@ final class MetsDocument extends AbstractDocument
     {
         // SimpleXMLElement objects can't be serialized, thus save the XML as string for serialization
         $this->asXML = $this->xml->asXML();
-        return ['pid', 'recordId', 'parentId', 'asXML'];
+        return ['pid', 'recordId', 'parentId', 'useGroupsConfiguration', 'asXML'];
     }
 
     /**