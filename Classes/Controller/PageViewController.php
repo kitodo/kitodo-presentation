53c09b2736fbb07649f935bd44eadd0b8801a6da
Markus Weigelt <Markus.Weigelt@slub-dresden.de>
2025-04-24 14:50:25 +0200

    Update PageViewController.php

--- Classes/Controller/PageViewController.php
+++ Classes/Controller/PageViewController.php
@@ -175,7 +175,7 @@ class PageViewController extends AbstractController
         $docNumPages = [];
         $i = 0;
         foreach ($this->documentArray as $document) {
-            if (!array_key_exists($i, $this->requestData['docPage']) ) {
+            if (is_null($document) || !array_key_exists($i, $this->requestData['docPage']) ) {
                 continue;
             }
 
