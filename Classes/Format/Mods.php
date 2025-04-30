954896c61b0dfb5348574816802f3398e38b04e8
Beatrycze Volk <beatrycze.volk@slub-dresden.de>
2025-04-30 11:40:08 +0200

    [BUGFIX] Display author URI as an external link (#1569)
    Co-authored-by: Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>

--- Classes/Format/Mods.php
+++ Classes/Format/Mods.php
@@ -177,11 +177,16 @@ class Mods implements MetadataInterface
                 $k++;
             }
             ksort($name);
+
             $this->metadata['author'][$i] = trim(implode(', ', $name));
         }
-        // Append "valueURI" to name using Unicode unit separator.
+
         if (!empty($authors[$i]->getValueURI())) {
-            $this->metadata['author'][$i] .= pack('C', 31) . $authors[$i]->getValueURI();
+            $displayName = $this->metadata['author'][$i];
+            $this->metadata['author'][$i] = [
+                'name' => $displayName,
+                'url' => (string) $authors[$i]->getValueURI()
+            ];
         }
     }
 
@@ -265,9 +270,13 @@ class Mods implements MetadataInterface
     private function getHolderFromXml(array $holders, int $i): void
     {
         $this->getHolderFromXmlDisplayForm($holders, $i);
-        // Append "valueURI" to name using Unicode unit separator.
+        
+        $displayName = $this->metadata['holder'][$i];
         if (!empty($holders[$i]->getValueURI())) {
-            $this->metadata['holder'][$i] .= pack('C', 31) . $holders[$i]->getValueURI();
+            $this->metadata['author'][$i] = [
+                'name' => $displayName,
+                'url' => (string) $holders[$i]->getValueURI()
+            ];
         }
     }
 
