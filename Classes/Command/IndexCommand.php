655eaf1943148954b1541d6d6edf24c8a34a30fb
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2025-09-01 17:41:11 +0200

    Update Classes/Command/IndexCommand.php

--- Classes/Command/IndexCommand.php
+++ Classes/Command/IndexCommand.php
@@ -150,7 +150,7 @@ class IndexCommand extends BaseCommand
             if (MathUtility::canBeInterpretedAsInteger($input->getOption('owner'))) {
                 $this->owner = $this->libraryRepository->findByUid(MathUtility::forceIntegerInRange((int) $input->getOption('owner'), 1));
             } else {
-                $this->owner = $this->libraryRepository->findOneBy([ 'indexName' => (string) $input->getOption('owner')]);
+                $this->owner = $this->libraryRepository->findOneBy(['indexName' => (string) $input->getOption('owner')]);
             }
         } else {
             $this->owner = null;
