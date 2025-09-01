280cf0ecaaf467236cae2d58f97fabfed9e39c07
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2025-09-01 17:43:34 +0200

    Update Classes/Controller/BasketController.php

--- Classes/Controller/BasketController.php
+++ Classes/Controller/BasketController.php
@@ -641,7 +641,7 @@ class BasketController extends AbstractController
         $printerId = $this->requestData['print_action'];
 
         // get id from db and send selected doc download link
-        $printer = $this->printerRepository->findOneBy([ 'uid' => $printerId ]);
+        $printer = $this->printerRepository->findOneBy(['uid' => $printerId]);
 
         // printer is selected
         if ($printer) {
