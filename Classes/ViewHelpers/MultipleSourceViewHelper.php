559892add90880361528f64f33357fe995f2959c
Markus Weigelt <markus.weigelt@slub-dresden.de>
2025-09-08 14:02:49 +0200

    Remove multiple source from multiview grid

---
+++ Classes/ViewHelpers/MultipleSourceViewHelper.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * (c) Kitodo. Key to digital objects e.V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo and TYPO3 projects.
+ *
+ * @license GNU General Public License version 3 or later.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+namespace Kitodo\Dlf\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+class MultipleSourceViewHelper extends AbstractViewHelper
+{
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('docArrayKey', 'string', 'The array key of the doc array', true);
+    }
+
+    /**
+     * Checks if key has a multisource prefix
+     *
+     * @return int
+     */
+    public function render()
+    {
+        if (preg_match('/^multipleSource_(\d+)$/', $this->arguments['docArrayKey'], $matches)) {
+            return (int)$matches[1];
+        }
+        return -1;
+    }
+}
