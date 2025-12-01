kitodo/kitodo-presentation

commit f6faaefbe4516d23d63083aa59094b297fdacb15

[IMPROVEMENT] Replace multiview source remove link (#1749)

Classes/ViewHelpers/LinkViewHelper.php
@@ -48,22 +48,35 @@ final class LinkViewHelper extends AbstractTagBasedViewHelper
 
         $viewData = $this->arguments['viewData'];
 
-        // UriBuilder does not properly encode specified entities in URL parameter
-        // For more details, please see the following TYPO3 issue https://forge.typo3.org/issues/107026
-        if (isset($viewData['requestData']['id']) && GeneralUtility::isValidUrl($viewData['requestData']['id'])) {
-            $viewData['requestData']['id'] = str_replace("%2F", "%252F", $viewData['requestData']['id']);
-        }
-
-        $arguments = [];
-        foreach ($viewData['requestData'] as $key => $value) {
-            if (!in_array($key, $this->arguments['excludedParams'])) {
-                $arguments['tx_dlf'][$key] = $value;
+        $dlfArguments = [];
+        foreach ($viewData['requestData'] as $key => $data) {
+            if (is_array($data)) {
+                $tempData = [];
+                foreach ($data as $dataKey => $dataValue) {
+                    if (!in_array($key . '[' . $dataKey . ']', $this->arguments['excludedParams'])) {
+                        $tempData[] = $dataValue;
+                    }
+                }
+                if (count($tempData) > 0) {
+                    $dlfArguments[$key] = $tempData;
+                }
+            } elseif (!in_array($key, $this->arguments['excludedParams'])) {
+                $dlfArguments[$key] = $data;
             }
         }
 
         $additionalParams = $this->arguments['additionalParams'];
         foreach ($additionalParams as $key => $value) {
-            $arguments['tx_dlf'][$key] = $value;
+            $dlfArguments[$key] = $value;
+        }
+
+        // double replace encoding in URL value parameters
+        if (isset($dlfArguments['id'])) {
+            $dlfArguments['id'] = $this->doubleEncode($dlfArguments['id']);
+        }
+
+        if (isset($dlfArguments['multiViewSource']) && is_array($dlfArguments['multiViewSource'])) {
+            $dlfArguments['multiViewSource'] = array_map([$this, 'doubleEncode'], $dlfArguments['multiViewSource']);
         }
 
         $childContent = (string) $this->renderChildren();
@@ -77,7 +90,7 @@ final class LinkViewHelper extends AbstractTagBasedViewHelper
         }
 
         $uri = $uriBuilder
-            ->setArguments($arguments)
+            ->setArguments(['tx_dlf' => $dlfArguments])
             ->setArgumentPrefix('tx_dlf')
             ->uriFor('main');
 
@@ -91,5 +104,22 @@ final class LinkViewHelper extends AbstractTagBasedViewHelper
         $tag->tag->setContent($childContent);
 
         return $tag->tag->render();
+    }
+
+    /**
+     * Double encode specific characters in URL.
+     *
+     * UriBuilder does not properly encode specified entities in URL parameter
+     * For more details, please see the following TYPO3 issue https://forge.typo3.org/issues/107026
+     *
+     * @param string $url The URL in which specific characters should be encoded
+     * @return string The replaced URL
+     */
+    private function doubleEncode(string $url): string
+    {
+        if (GeneralUtility::isValidUrl($url)) {
+            $url = str_replace("%2F", "%252F", $url);
+        }
+        return $url;
     }
 }

Classes/Controller/PageViewController.php
@@ -98,12 +98,15 @@ class PageViewController extends AbstractController
         $this->loadDocument();
 
         if ($this->isDocMissingOrEmpty()) {
-            // if document is empty and current document type is a multi document type redirect to multi view
+            // if document is empty and current document type is a multi document type then redirect to multi view
             if (!$this->isDocMissing() &&
                 $this->isMultiDocumentType($this->document->getCurrentDocument()->tableOfContents[0]['type'])) {
                 return $this->multiviewRedirect();
             }
             return $this->htmlResponse();
+        } elseif (array_key_exists('multiViewSource', $this->requestData)) {
+            // if request data contains multiViewSource parameter then redirect to multi view
+            return $this->multiviewRedirect();
         }
 
         $this->setPage();
@@ -521,6 +524,10 @@ class PageViewController extends AbstractController
             'tx_dlf[page]' => $this->requestData['page'],
             'tx_dlf[multiview]' => 1
         ];
+
+        if (array_key_exists('multiViewSource', $this->requestData)) {
+            $arguments['tx_dlf[multiViewSource]'] = $this->requestData['multiViewSource'];
+        }
 
         $uri = $this->uriBuilder
             ->reset()

Resources/Private/Templates/MultiView/Main.html
@@ -10,6 +10,7 @@
 -->
 </f:comment>
 <html xmlns:f="http://typo3.org/ns/TYPO3/CMS/Fluid/ViewHelpers"
+      xmlns:kitodo="http://typo3.org/ns/Kitodo/Dlf/ViewHelpers"
       data-namespace-typo3-fluid="true">
 
 <div class="page-control">
@@ -45,9 +46,9 @@
                 <div class="gridstack-dragging-handle" title="{f:translate(key: 'multiview.dragging_label', extensionName: 'dfgviewer')}"></div>
                 <f:if condition="{document.sourceKey} >= 0">
                     <div class="removeDocument">
-                        <f:link.action addQueryString="untrusted" argumentsToBeExcludedFromQueryString="{0: 'tx_dlf[multiViewSource][{document.sourceKey}]', 1: 'tx_dlf[multiview]'}">
+                        <kitodo:link viewData="{viewData}" excludedParams="{0: 'multiViewSource[{document.sourceKey}]', 1: 'multiview'}">
                             <f:translate key="multiview.remove_document" extensionName="dfgviewer" />
-                        </f:link.action>
+                        </kitodo:link>
                     </div>
                 </f:if>
             </div>

commit f6e15b1fe4155fca0b639e292167926b191dd6b0

[BUGFIX] Add missing type cast
This fixes a regression (issue #1757).

Fixes: f6faaefbe451 ("Replace multiview source remove link (#1749)")
Signed-off-by: Stefan Weil <sw@weilnetz.de>

Classes/ViewHelpers/LinkViewHelper.php
@@ -72,7 +72,7 @@ final class LinkViewHelper extends AbstractTagBasedViewHelper
 
         // double replace encoding in URL value parameters
         if (isset($dlfArguments['id'])) {
-            $dlfArguments['id'] = $this->doubleEncode($dlfArguments['id']);
+            $dlfArguments['id'] = $this->doubleEncode((string) $dlfArguments['id']);
         }
 
         if (isset($dlfArguments['multiViewSource']) && is_array($dlfArguments['multiViewSource'])) {