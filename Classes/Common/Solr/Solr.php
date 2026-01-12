946bebf302548b0e65419b3560896064381b9c63
Michael Kubina <michael.kubina@sub.uni-hamburg.de>
2026-01-12 11:07:08 +0100

    fix failing test probably due to camel-case field definition in ext_conf_template

--- Classes/Common/Solr/Solr.php
+++ Classes/Common/Solr/Solr.php
@@ -256,7 +256,7 @@ class Solr implements LoggerAwareInterface
             self::$fields['type'] = $solrFields['type'];
             self::$fields['title'] = $solrFields['title'];
             self::$fields['volume'] = $solrFields['volume'];
-            self::$fields['structure_path'] = $solrFields['structure_path'];
+            self::$fields['structure_path'] = $solrFields['structurePath'];
             self::$fields['date'] = $solrFields['date'] ?? null;
             self::$fields['thumbnail'] = $solrFields['thumbnail'];
             self::$fields['default'] = $solrFields['default'];
