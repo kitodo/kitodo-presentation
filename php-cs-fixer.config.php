689fb494fbbdf5fef7c12e8fefa5bafc73357d84
Marco Lengua <marco.lengua.hinojosa@sub.uni-hamburg.de>
2026-03-12 09:12:14 +0100

    more path included PSR12 fixes

--- php-cs-fixer.config.php
+++ php-cs-fixer.config.php
@@ -1,18 +1,13 @@
 <?php
-
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__)
-    ->exclude('.github')
-    ->exclude('BUILD')
-    ->exclude('Configuration')
-    ->exclude('Resources')
-    ->exclude('Tests')
-    ->exclude('Classes/Api')
-    ->exclude('Documentation')
-    ->notPath(['php-cs-fixer.config.php']);
+->in([
+    __DIR__ . '/Classes',
+    __DIR__ . '/Configuration',
+    __DIR__ . '/Tests',
+]);
 
 return (new PhpCsFixer\Config())
-    ->setRules([
-        '@PSR12' => true,
-    ])
-    ->setFinder($finder);
+->setRules([
+    '@PSR12' => true,
+])
+->setFinder($finder);
