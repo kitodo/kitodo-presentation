7b2bcf2bdc091ef151d75f77fb21d708a3bda24e
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2026-03-16 09:32:23 +0100

    Update php-cs-fixer.config.php

--- php-cs-fixer.config.php
+++ php-cs-fixer.config.php
@@ -1,13 +1,26 @@
 <?php
-$finder = PhpCsFixer\Finder::create()
-->in([
-    __DIR__ . '/Classes',
-    __DIR__ . '/Configuration',
-    __DIR__ . '/Tests',
-]);
 
-return (new PhpCsFixer\Config())
-->setRules([
-    '@PSR12' => true,
-])
-->setFinder($finder);
+/**
+ * (c) Kitodo. Key to digital objects e.V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo and TYPO3 projects.
+ *
+ * @license GNU General Public License version 3 or later.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+namespace PhpCsFixer;
+
+$finder = Finder::create()
+    ->in([
+        __DIR__ . '/Classes',
+        __DIR__ . '/Configuration',
+        __DIR__ . '/Tests',
+    ]);
+
+return (new Config())
+    ->setRules([
+        '@PSR12' => true,
+    ])
+    ->setFinder($finder);
