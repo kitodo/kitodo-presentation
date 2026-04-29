52cb10c5a696b13206a526def7a52deec040ff86
Beatrycze Volk <beatrycze.volk@slub-dresden.de>
2026-04-29 16:14:14 +0200

    [MAINTENANCE] Add query methods for record visibility and standardize default settings
    Adds `ignoreDeleted()`, `showDeleted()`, `ignoreHidden()`, and `showHidden()` methods to `AbstractRepository`. These methods provide explicit control over whether deleted or hidden records are included in subsequent queries.
    
    Furthermore, it introduces a `getDefaultQuerySettings()` helper method to centralize the retrieval and manipulation of default query settings. Existing methods like `ignoreStoragePid()` and `useStoragePid()` are updated to leverage this new helper, improving consistency and maintainability of query setting management.

--- Classes/Domain/Repository/AbstractRepository.php
+++ Classes/Domain/Repository/AbstractRepository.php
@@ -13,6 +13,7 @@
 namespace Kitodo\Dlf\Domain\Repository;
 
 use TYPO3\CMS\Extbase\DomainObject\DomainObjectInterface;
+use TYPO3\CMS\Extbase\Persistence\Generic\QuerySettingsInterface;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 
 /**
@@ -29,6 +30,34 @@ use TYPO3\CMS\Extbase\Persistence\Repository;
 class AbstractRepository extends Repository
 {
     /**
+     * Sets deleted records to be not returned by the find methods in the repository.
+     *
+     * @access public
+     *
+     * @return void
+     */
+    public function ignoreDeleted(): void
+    {
+        $querySettings = $this->getDefaultQuerySettings();
+        $querySettings->setIncludeDeleted(false);
+        $this->setDefaultQuerySettings($querySettings);
+    }
+
+    /**
+     * Sets hidden records to be not returned by the find methods in the repository.
+     *
+     * @access public
+     *
+     * @return void
+     */
+    public function ignoreHidden(): void
+    {
+        $querySettings = $this->getDefaultQuerySettings();
+        $querySettings->setIgnoreEnableFields(false);
+        $this->setDefaultQuerySettings($querySettings);
+    }
+
+    /**
      * Sets the storage pid to be ignored in the repository.
      *
      * @access public
@@ -37,7 +66,7 @@ class AbstractRepository extends Repository
      */
     public function ignoreStoragePid(): void
     {
-        $querySettings = $this->createQuery()->getQuerySettings();
+        $querySettings = $this->getDefaultQuerySettings();
         $querySettings->setRespectStoragePage(false);
         $this->setDefaultQuerySettings($querySettings);
     }
@@ -53,9 +82,51 @@ class AbstractRepository extends Repository
      */
     public function useStoragePid(int $storagePid): void
     {
-        $querySettings = $this->createQuery()->getQuerySettings();
+        $querySettings = $this->getDefaultQuerySettings();
         $querySettings->setRespectStoragePage(true);
         $querySettings->setStoragePageIds([$storagePid]);
         $this->setDefaultQuerySettings($querySettings);
+    }
+
+    /**
+     * Sets deleted records to be returned by the find methods in the repository.
+     *
+     * @access public
+     *
+     * @return void
+     */
+    public function showDeleted(): void
+    {
+        $querySettings = $this->getDefaultQuerySettings();
+        $querySettings->setIncludeDeleted(true);
+        $this->setDefaultQuerySettings($querySettings);
+    }
+
+    /**
+     * Sets hidden records to be not returned by the find methods in the repository.
+     *
+     * @access public
+     *
+     * @return void
+     */
+    public function showHidden(): void
+    {
+        $querySettings = $this->getDefaultQuerySettings();
+        $querySettings->setIgnoreEnableFields(true);
+        $this->setDefaultQuerySettings($querySettings);
+    }
+
+    /**
+     * Get default settings for repository, if not set create the new one.
+     *
+     * @return QuerySettingsInterface
+     */
+    protected function getDefaultQuerySettings(): QuerySettingsInterface
+    {
+        // @phpstan-ignore-next-line (defaultQuerySettings can be null)
+        if (empty($this->defaultQuerySettings)) {
+            return $this->createQuery()->getQuerySettings();
+        }
+        return $this->defaultQuerySettings;
     }
 }
